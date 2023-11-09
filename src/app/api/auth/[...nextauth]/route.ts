f391ca6377cf4229f46a5483a9eaba94a91d0ef7
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-09 10:12:51 +0800

    stored the unsigned up user in db

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,9 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "@/lib/prisma"
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
 
 const authOptions: NextAuthOptions = {
-    // adapter: PrismaAdapter(prisma),
     session: {
         strategy: "jwt"
     },
@@ -16,16 +15,25 @@ const authOptions: NextAuthOptions = {
     ],
 
     callbacks: {
-        async jwt ({ user, token }) {    
-            if (user) {     
-                token.picture = user.image?.toString()
-                token.id = user.email?.toString()
-            }
+        async jwt ({ user, token }) {   
+            const existingUser = await prisma.user.findFirst({
+              where: {
+                email: user?.email as string
+              }
+            })   
+            
+            // if the user is existing, pass the existing profile and id from db
+            if (existingUser) {     
+                token.picture = existingUser.image?.image_url as string
+                token.id = existingUser.id
+            }    
 
             return token
         },
+
         async session({ session, token }) {
-            if (token && session.user) {
+            if (session.user) {
+              //pass the attached user credentials of token in session
               session.user.id = token.id as string
               session.user.name = token.name;
               session.user.email = token.email;
@@ -34,11 +42,42 @@ const authOptions: NextAuthOptions = {
 
             return session;
         },
+        async signIn ({ user }){
+          try {
+            // checks if the user is already in db
+            const existingUser = await prisma.user.findFirst({
+              where: {
+                email: user?.email as string
+              }
+            }) 
+
+            if(!existingUser) {
+              //creates a new user if it is not stored yet
+              await prisma.user.create({
+                data: {
+                  email: user?.email as string,
+                  name: user?.name,
+                  image: {
+                    image_url: user.image as string
+                  } 
+                }
+              })
+            }
+
+            return true
+            
+          } catch (error) {
+            NextResponse.json({
+              messsage: "Internal Server Error"
+            },{status: 500})
+
+            return false
+          }
+        
+        }
     }
 }
 
 const authHandler = NextAuth(authOptions)
 
 export { authHandler as GET, authHandler as POST }
-
-
