0b76ab8e8db22e82ac477fbc3e8a6895ef3cbc9d
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-10 20:28:13 +0800

    change prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,19 @@
-import { PrismaClient } from "@prisma/client";
-//prisma client is used for reading, manipulating data
+import { PrismaClient } from "@prisma/client"
+
 declare global {
-  var prisma: PrismaClient | undefined;
-};
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient
+}
 
-export const prisma = globalThis.prisma || new PrismaClient();
+let prisma: PrismaClient
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient()
+} else {
+    if (!global.cachedPrisma) {
+      global.cachedPrisma = new PrismaClient()
+    }
+    prisma = global.cachedPrisma
+}
+
+export const db = prisma
