f14bfbe92819ae352dc094ffa15ee2746f873969
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-10 20:29:21 +0800

    fix jwt, added prisma adpater and remove sign in callback

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,83 +1,58 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { prisma } from "@/lib/prisma";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { db } from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-const authOptions: NextAuthOptions = {
-    session: {
-        strategy: "jwt"
-    },
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID!,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      }),
-    ],
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
 
-    callbacks: {
-        async jwt ({ user, token }) {   
-            const existingUser = await prisma.user.findFirst({
-              where: {
-                email: user?.email as string
-              }
-            })   
-            
-            // if the user is existing, pass the existing profile and id from db
-            if (existingUser) {     
-                token.picture = existingUser.image?.image_url as string
-                token.id = existingUser.id
-            }    
-
-            return token
+  callbacks: {
+    async jwt({ token, user }) {
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email,
         },
+      })
 
-        async session({ session, token }) {
-            if (session.user) {
-              //pass the attached user credentials of token in session
-              session.user.id = token.id as string
-              session.user.name = token.name;
-              session.user.email = token.email;
-              session.user.image =  token.picture;
-            }
-
-            return session;
-        },
-        async signIn ({ user }){
-          try {
-            // checks if the user is already in db
-            const existingUser = await prisma.user.findFirst({
-              where: {
-                email: user?.email as string
-              }
-            }) 
-
-            if(!existingUser) {
-              //creates a new user if it is not stored yet
-              await prisma.user.create({
-                data: {
-                  email: user?.email as string,
-                  name: user?.name,
-                  image: {
-                    image_url: user.image as string
-                  } 
-                }
-              })
-            }
-
-            return true
-            
-          } catch (error) {
-            NextResponse.json({
-              messsage: "Internal Server Error"
-            },{status: 500})
-
-            return false
-          }
-        
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id
         }
-    }
-}
+        return token
+      }
 
-const authHandler = NextAuth(authOptions)
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      }
+    },
 
-export { authHandler as GET, authHandler as POST }
+    async session({ session, token }) {
+      if (session.user) {
+        //pass the attached user credentials of token in session
+        session.user.id = token.id as string;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+
+      return session;
+    },
+  },
+};
+
+const authHandler = NextAuth(authOptions);
+
+export { authHandler as GET, authHandler as POST };
