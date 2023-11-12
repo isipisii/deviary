e08c21c16d3d8b502f70c00f9f8f46953ba8ec96
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-12 15:46:46 +0800

    create separate file for auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,57 +1,5 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { db } from "@/lib/prisma";
-import { NextResponse } from "next/server";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(db),
-  session: {
-    strategy: "jwt",
-  },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-
-  callbacks: {
-    async jwt({ token, user }) {
-      const dbUser = await db.user.findFirst({
-        where: {
-          email: token.email,
-        },
-      })
-
-      if (!dbUser) {
-        if (user) {
-          token.id = user?.id
-        }
-        return token
-      }
-
-      return {
-        id: dbUser.id,
-        name: dbUser.name,
-        email: dbUser.email,
-        picture: dbUser.image,
-      }
-    },
-
-    async session({ session, token }) {
-      if (session.user) {
-        //pass the attached user credentials of token in session
-        session.user.id = token.id as string;
-        session.user.name = token.name;
-        session.user.email = token.email;
-        session.user.image = token.picture;
-      }
-
-      return session;
-    },
-  },
-};
+import { authOptions } from "@/lib/auth";
 
 const authHandler = NextAuth(authOptions);
 
---
+++ src/lib/auth.ts
@@ -0,0 +1,57 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { db } from "@/lib/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { getServerSession } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      const existingUser = await db.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      })
+
+      if (!existingUser) {
+        if (user) {
+          token.id = user?.id
+        }
+        return token
+      }
+
+      return {
+        id: existingUser.id,
+        name: existingUser.name,
+        email: existingUser.email,
+        picture: existingUser.image,
+      }
+    },
+
+    async session({ session, token }) {
+      if (session.user) {
+        //pass the attached user credentials of token in session
+        session.user.id = token.id as string;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+
+      return session;
+    },
+  },
+};
+
+// for getting server session without putting authOptions in every declaration
+export const getServerSideSession = () => getServerSession(authOptions)
