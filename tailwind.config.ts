32d8738de6a7323ce0f25650508bcdf757fdad7e
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-12 15:44:48 +0800

    install and set-up nextui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,71 @@
 import type { Config } from 'tailwindcss'
+const {nextui} = require("@nextui-org/react");
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  // theme: {
+  //   extend: {
+  //     backgroundImage: {
+  //       'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  //       'gradient-conic':
+  //         'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  //     },
+  //   },
+  // },
+  plugins: [
+    nextui({
+      themes: {
+        dark: {
+          extend: "dark", // <- inherit default values from dark theme
+          colors: {
+            background: "#00060C",
+            foreground: "#ffffff",
+            primary: {
+              50: "#3B096C",
+              100: "#520F83",
+              200: "#7318A2",
+              300: "#9823C2",
+              400: "#c031e2",
+              500: "#DD62ED",
+              600: "#F182F6",
+              700: "#FCADF9",
+              800: "#FDD5F9",
+              900: "#FEECFE",
+              DEFAULT: "#DD62ED",
+              foreground: "#ffffff",
+            },
+            borderColor: "#232323",
+            focus: "#F182F6",
+          },
+          layout: {
+            disabledOpacity: "0.3",
+            radius: {
+              small: "4px",
+              medium: "6px",
+              large: "8px",
+            },
+            borderWidth: {
+              small: "1px",
+              medium: "2px",
+              large: "3px",
+            },
+          },
+        },
+        light: {
+          extend: "light",
+          colors: {
+            borderColor: "#DBDBDB",
+          },
+        },
       },
-    },
-  },
-  plugins: [],
+    }),
+  ],
+  darkMode: "class",
+
 }
 export default config
