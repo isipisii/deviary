3883911bc7b5f8c3491f805349f8a78bed79db05
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-13 23:18:50 +0800

    add logo and current user drop down

--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -1,35 +1,42 @@
-/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { signIn, signOut, getProviders, useSession } from "next-auth/react";
 import { useTheme } from "next-themes";
 import AccountDropdown from "./ui/account-dropdown";
+import Image from "next/image";
 
 export default function TopNav() {
   const { theme } = useTheme();
   const { data, status } = useSession();
-
   async function logIn() {
     await signIn("google");
   }
 
+  console.log(theme);
   return (
     <nav className="border border-borderColor">
       <div className="flex p-4 justify-between">
         {/* logo */}
-        {theme === 'light' ? (
-          <img src="/assets/deviary-light.svg" alt="logo" />
+        {/*theres a bug in ternary thats why i conditionally rendered it */}
+        {theme === "light" ? (
+          <img
+            src="/images/deviary-light.svg"
+            alt="logo"
+          />
         ) : (
-          <img src="/assets/deviary-dark.svg" alt="logo" />  
+          <img
+            src="/images/deviary-dark.svg"
+            alt="logo"
+          />
         )}
 
         <div>
-          {/* TODO: make a context menu for sign out*/}
-          {!data && <button onClick={logIn}>Sign in with google</button>}
+          {status === "unauthenticated" && (
+            <button onClick={logIn}>Sign in with google</button>
+          )}
           <AccountDropdown />
         </div>
       </div>
     </nav>
   );
 }
-  
