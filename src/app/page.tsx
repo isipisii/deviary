c3a1f42e601f9d18f6aa5ce48c1f419c7f105ec9
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-19 15:32:26 +0800

    add session checker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import ThemeToggler from "@/components/ui/theme-toggler"
 import { getServerSideSession } from "@/lib/auth"
+import { redirect } from "next/navigation"
 
 export default async function Home() {
   const session = await getServerSideSession()
-  console.log(session)
+  
+  if(!session) redirect("/sign-in")
 
   return (
    <main> 
