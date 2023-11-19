8c8666282ad535aee72d611dabffb88978be38d6
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-19 15:33:42 +0800

    remove the button for signing in using google

--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -1,41 +1,28 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { signIn, signOut, getProviders, useSession } from "next-auth/react";
 import { useTheme } from "next-themes";
 import AccountDropdown from "./ui/account-dropdown";
-import Image from "next/image";
 
 export default function TopNav() {
   const { theme } = useTheme();
   const { data, status } = useSession();
+  
   async function logIn() {
     await signIn("google");
   }
 
-  console.log(theme);
   return (
     <nav className="border border-borderColor">
       <div className="flex p-4 justify-between">
         {/* logo */}
         {/*theres a bug in ternary thats why i conditionally rendered it */}
-        {theme === "light" ? (
-          <img
-            src="/images/deviary-light.svg"
-            alt="logo"
-          />
-        ) : (
-          <img
-            src="/images/deviary-dark.svg"
-            alt="logo"
-          />
-        )}
-
-        <div>
-          {status === "unauthenticated" && (
-            <button onClick={logIn}>Sign in with google</button>
-          )}
-          <AccountDropdown />
-        </div>
+        <img
+          src={theme === "light" ? "/images/deviary-light.svg" :  "/images/deviary-dark.svg" }
+          alt="logo"
+        />
+        <AccountDropdown />
       </div>
     </nav>
   );
