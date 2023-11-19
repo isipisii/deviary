db2118c276860f7a9d8f9eba544bf0f92cf2ee47
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-19 15:35:37 +0800

    some config stuff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,10 +10,9 @@ const config: Config = {
   ],
   // theme: {
   //   extend: {
-  //     backgroundImage: {
-  //       'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-  //       'gradient-conic':
-  //         'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  //     fontFamily: {
+  //       sans: ['var(--font-geist-sans)'],
+  //       mono: ['var(--font-geist-mono)'],
   //     },
   //   },
   // },
@@ -60,6 +59,19 @@ const config: Config = {
           extend: "light",
           colors: {
             borderColor: "#DBDBDB",
+          },
+          layout: {
+            disabledOpacity: "0.3",
+            radius: {
+              small: "4px",
+              medium: "6px",
+              large: "8px",
+            },
+            borderWidth: {
+              small: "1px",
+              medium: "2px",
+              large: "3px",
+            },
           },
         },
       },
