5fc661c62b294b488be7ba0b6601620a57b2fc94
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-22 23:14:33 +0800

    create middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,59 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt"
+import { withAuth } from "next-auth/middleware"
+import { NextResponse, NextRequest } from "next/server"
+// import { getServerSideSession } from "./lib/auth"
 
-export async function middleware(req: NextRequest) {
-  const token = await getToken({ req });
+// using with auth from next auth
+export default withAuth(
+  async function middleware(req) {
+    const token = await getToken({ req })
+    const isAuth = !!token
+    const isAuthPage =
+    req.nextUrl.pathname.startsWith("/sign-in") ||
+    req.nextUrl.pathname.startsWith("/sign-up")
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/sign-in", req.nextUrl));
+
+    if (isAuthPage) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL("/", req.url))
+      }
+
+      return null
+    }
+
+    if (!isAuth) {
+      return NextResponse.redirect(
+        new URL("/sign-in", req.url)
+      );
+    }
+  },
+  {
+    callbacks: {
+      async authorized() {
+        // This is a work-around for handling redirect on auth pages.
+        // We return true here so that the middleware function above
+        // is always called.
+        return true
+      },
+    },
   }
-}
+)
 
-// See "Matching Paths" below to learn more
+// using server session
+// export async function middleware(request: NextRequest) {
+//   const session = await getServerSideSession()
+//   const isAuthenticated = !!session?.user
+//   const isInAuthPage = request.nextUrl.pathname.startsWith("/sign-in") || request.nextUrl.pathname.startsWith("/sign-up")
+  
+//   if(isAuthenticated && isInAuthPage) {
+//     return NextResponse.redirect(new URL('/', request.url))
+//   }
+
+//   return NextResponse.redirect(new URL('/sign-in', request.url))
+
+// }
+ 
 export const config = {
-  matcher: ["/sign-in", "/sign-up", "/guild", ],
-};
+  // these are the paths where the middleware will run
+  matcher: ["/", "/sign-in", "/sign-up", "/guild", "/onboarding", "/feed"]
+}
