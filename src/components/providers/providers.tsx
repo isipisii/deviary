970b206d2e835038b5aa4de65115df05ed25262e
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-22 23:17:32 +0800

    add query client provider

--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -1,19 +1,25 @@
 "use client"
-
+import { useState } from "react";
 import { ReactNode } from "react"
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from "next-themes";
 import { NextUIProvider } from '@nextui-org/react'
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function Providers({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient())
+
   return (
     <SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
         <NextUIProvider>
             <ThemeProvider>
-                {children} 
+              {children} 
             </ThemeProvider>
         </NextUIProvider>
+      </QueryClientProvider>
     </SessionProvider>
   )
 }
