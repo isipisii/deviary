ea36ae222c5969cebd57a2102427d30c2fce2743
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-22 23:19:00 +0800

    create api route for sign up

---
+++ src/app/api/auth/sign-up/route.ts
@@ -0,0 +1,59 @@
+import { db } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import argon2 from "argon2";
+import { signUpSchema } from "@/lib/validators/auth-validator";
+import z from "zod";
+
+type TSignUpSchema = z.infer<typeof signUpSchema>
+//creates new user
+export const POST = async (request: NextRequest) => {
+    const body: TSignUpSchema= await request.json()
+    const { email, password: rawPassword, confirmPassword } = body
+    
+    const signUpCredentials = signUpSchema.safeParse({
+        email,
+        password: rawPassword,
+        confirmPassword
+    })
+
+    try {
+        if(!signUpCredentials.success) {
+            return NextResponse.json({
+                errors: signUpCredentials.error.flatten().fieldErrors,
+                message: "Error in sign up credentials.",
+            }, { status: 400 })
+        }
+
+        const existingUserEmail = await db.user.findFirst({
+            where: {
+                email
+            }
+        })
+
+        if(existingUserEmail) {
+            return NextResponse.json({
+                success: false,
+                message: "The email you provided is already existing",
+            }, { status: 400 })
+        }
+
+        const hashedPassword = await argon2.hash(rawPassword)
+
+        const user = await db.user.create({
+            data: {
+                email: email,
+                password: hashedPassword
+            }
+        })
+
+        return NextResponse.json({
+            data: user,
+            message: "successful"
+        }, { status: 201 })
+
+    } catch (error) {
+        NextResponse.json({
+            message: "Internal Server Error"
+        }, {status: 500})
+    }
+}
