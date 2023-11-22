97a4463da8004ca83dd14b0e62726ee1e8268c08
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-22 23:16:08 +0800

    change toggler

--- src/components/ui/theme-toggler.tsx
+++ src/components/ui/theme-toggler.tsx
@@ -2,7 +2,9 @@
 
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import { Button } from "@nextui-org/react";
+import { Switch } from "@nextui-org/react";
+import { FaMoon } from "react-icons/fa";
+import { IoSunny } from "react-icons/io5";
 
 export default function ThemeToggler() {
   const [mounted, setMounted] = useState(false)
@@ -16,13 +18,17 @@ export default function ThemeToggler() {
 
   return (
     <div>
-      The current theme is: {theme}
-      <Button color="secondary" variant="solid" onClick={() => setTheme('light')}>
-        Light Mode
-      </Button>
-      <Button color="secondary" variant="flat" onClick={() => setTheme('dark')}>
-        Dark Mode
-      </Button>
+      <Switch
+        isSelected={theme === "dark"}
+        onValueChange={() => setTheme(theme === "dark" ? "light ": "dark")}
+        thumbIcon={({ isSelected, className }) =>
+        isSelected ? (
+          <IoSunny className={className} />
+        ) : (
+          <FaMoon className={className} />
+        )
+      }
+      />
     </div>
   )
 };
