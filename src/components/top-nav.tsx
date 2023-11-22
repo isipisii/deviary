6e41a144a00d27547cc39f8333230e62b99c51b5
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-22 23:16:42 +0800

    mount the theme toggler

--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -1,28 +1,37 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { signIn, signOut, getProviders, useSession } from "next-auth/react";
 import { useTheme } from "next-themes";
 import AccountDropdown from "./ui/account-dropdown";
+import { FiBell } from "react-icons/fi";
+import { Button, Tooltip } from "@nextui-org/react";
+import ThemeToggler from "./ui/theme-toggler";
 
 export default function TopNav() {
   const { theme } = useTheme();
-  const { data, status } = useSession();
-  
-  async function logIn() {
-    await signIn("google");
-  }
 
   return (
     <nav className="border border-borderColor">
-      <div className="flex p-4 justify-between">
+      <div className="flex p-4 justify-between items-center">
         {/* logo */}
-        {/*theres a bug in ternary thats why i conditionally rendered it */}
         <img
           src={theme === "light" ? "/images/deviary-light.svg" :  "/images/deviary-dark.svg" }
           alt="logo"
         />
-        <AccountDropdown />
+
+        <div className="flex gap-4 items-center">
+            <ThemeToggler />
+            <Tooltip content="Notifications" className="bg-backgorund">
+              <Button 
+                variant="bordered" 
+                isIconOnly 
+                className="border-borderColor border-1 rounded-xl text-[1.3rem]" 
+              >
+                <FiBell />
+              </Button>
+            </Tooltip>
+            <AccountDropdown />
+        </div>
       </div>
     </nav>
   );
