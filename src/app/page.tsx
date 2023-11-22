05345200edc12d3d0c1e72a3dc3dc2fbf766245e
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-22 23:18:01 +0800

    add onboarded checker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 import ThemeToggler from "@/components/ui/theme-toggler"
 import { getServerSideSession } from "@/lib/auth"
 import { redirect } from "next/navigation"
+import { db } from "@/lib/prisma"
 
 export default async function Home() {
   const session = await getServerSideSession()
-  
-  if(!session) redirect("/sign-in")
 
+  const user = await db.user.findFirst({
+    where: {
+      id: session?.user.id
+    }
+  })
+
+  if(user) {
+    if(!user.onboarded) {
+      redirect("/onboarding")
+    }
+  }
+  
   return (
    <main> 
-      <ThemeToggler /> 
+      
    </main>
   )
 }
