81f56e13897d1778c9ed7616d74eacb971137a9f
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-22 23:16:28 +0800

    remove conditional rendering

--- src/components/ui/account-dropdown.tsx
+++ src/components/ui/account-dropdown.tsx
@@ -7,9 +7,7 @@ import {
   DropdownTrigger,
   DropdownMenu,
   DropdownItem,
-  Button,
   User,
-  Avatar,
   DropdownSection,
 } from "@nextui-org/react";
 import { signOut } from "next-auth/react";
@@ -18,43 +16,41 @@ export default function AccountDropdown() {
   const { data, status } = useSession();
   const isAuthenticated = status === "authenticated"
 
+  // TODO: make a loader
   return (
-    <>
-      {data && data.user.image && isAuthenticated && 
-        <Dropdown placement="bottom-start" backdrop="transparent" className="bg-background">
-          <DropdownTrigger>
-            <User
-              as="button"
-              avatarProps={{
-                src: data.user.image,
-              }}
-              className="transition-transform"
-              description={data.user.email}
-              name={data.user.name}
-            />
-          </DropdownTrigger>
-          <DropdownMenu aria-label="User Actions" variant="flat">
-            <DropdownSection showDivider>
-              <DropdownItem key="profile">
-                Profile
-              </DropdownItem>
-              <DropdownItem key="settings">
-                My Settings
-              </DropdownItem>
-            </DropdownSection>
-            <DropdownItem 
-              key="logout" 
-              color="danger" 
-              onClick={async () =>  {
-                await signOut()
-              }}
-              className="text-danger"
-            >
-              Log Out
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      }
-    </>
+    <Dropdown placement="bottom-start" backdrop="transparent" className="bg-background">
+      <DropdownTrigger>
+        <User
+          as="button"
+          avatarProps={{
+            src: data?.user?.image ?? "",
+          }}
+          className="transition-transform"
+          description={data?.user.email}
+          name={data?.user.name}
+        />
+      </DropdownTrigger>
+      <DropdownMenu aria-label="User Actions" variant="flat">
+        <DropdownSection showDivider>
+          <DropdownItem key="profile">
+            Profile
+          </DropdownItem>
+          <DropdownItem key="settings">
+            My Settings
+          </DropdownItem>
+        </DropdownSection>
+        <DropdownItem 
+          key="logout" 
+          color="danger" 
+          onClick={async () =>  {
+            await signOut()
+            
+          }}
+          className="text-danger"
+        >
+          Log Out
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
   );
 }
