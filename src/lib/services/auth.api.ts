411179045eb2d33df8e410e2943bc43345e385df
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-28 15:58:12 +0800

    change fetch to axios

--- src/lib/services/auth.api.ts
+++ src/lib/services/auth.api.ts
@@ -1,18 +1,24 @@
 import { TSignUpSchema } from "@/app/(auth)/components/sign-up-form";
+import axios from "axios";
+
+// export const signUp = async (formData: TSignUpSchema) => {
+//     const response = await fetch("api/auth/sign-up", {
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       method: "POST",
+//       body: JSON.stringify(formData)
+//     })
+
+//     if (!response.ok) {
+//       const error = await response.json();
+//       throw new Error(error.message);
+//     }  
+
+//     return response.json()
+// }
+
 
 export const signUp = async (formData: TSignUpSchema) => {
-    const response = await fetch("api/auth/sign-up", {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-      body: JSON.stringify(formData)
-    })
-
-    if (!response.ok) {
-      const error = await response.json();
-      throw new Error(error.message);
-    }  
-
-    return response.json()
+   return await axios.post("/api/auth/sign-up", formData)
 }
