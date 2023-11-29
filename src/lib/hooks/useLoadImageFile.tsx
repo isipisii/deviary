8f79b9e44bd02dd1bccf4e78b31dfe3b9eacada3
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-29 14:05:14 +0800

    create custom hook for loading image

---
+++ src/lib/hooks/useLoadImageFile.tsx
@@ -0,0 +1,29 @@
+import { ChangeEvent, useState } from "react"
+
+export default function useLoadImageFile() {
+    const [selectedImageFile, setSelectedImageFile] = useState<File | null>(null)
+    const [selectedImage, setSelectedImage] = useState("")
+
+    function handleFileChange(e: ChangeEvent<HTMLInputElement>){
+        const file = e.target.files?.[0]
+    
+        setSelectedImage(file ? URL.createObjectURL(file) : "")
+        setSelectedImageFile(file ?? null)
+    }
+
+    function handleRemoveImage() {
+        setSelectedImage("")
+        setSelectedImageFile(null)
+    }
+
+    return {
+        handleFileChange,
+        selectedImage,
+        selectedImageFile,
+        handleRemoveImage
+    }
+}
+
+
+  
+
