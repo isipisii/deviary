4e789a21cce267b754ab4851c0454b39d3c7fc60
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-30 19:18:36 +0800

    set-up progress bar

--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "next-themes";
 import { NextUIProvider } from '@nextui-org/react'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 
 export default function Providers({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient())
@@ -16,6 +17,12 @@ export default function Providers({ children }: { children: ReactNode }) {
         <ReactQueryDevtools initialIsOpen={false} />
         <NextUIProvider>
             <ThemeProvider>
+              <ProgressBar
+                height="3px"
+                color="#DD0DB9"
+                options={{ showSpinner: false }}
+                shallowRouting
+              />
               {children} 
             </ThemeProvider>
         </NextUIProvider>
