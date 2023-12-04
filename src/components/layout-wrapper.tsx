6b3b60a2d9e87d9a38d86ceb6eaa442993776d74
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-04 15:53:02 +0800

    remove margin in small devices

--- src/components/layout-wrapper.tsx
+++ src/components/layout-wrapper.tsx
@@ -7,7 +7,7 @@ export default function LayoutWrapper({ children }: { children: ReactNode }) {
   const { isSideBarMinimized } = useSideBarNavStore(state => state)
   
   return (
-    <main className={`${isSideBarMinimized ? "ml-[120px]": "ml-[300px]"} transition-all ease-in-out duration-300 mt-[60px]`} >
+    <main className={`${isSideBarMinimized ? "md:ml-[90px]": "md:ml-[300px]"} transition-all ease-in-out duration-300 mt-[60px]`} >
         {children}
     </main>
   )
