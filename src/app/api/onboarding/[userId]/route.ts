88901a12da782a00cdaf6c6f3f606417d6ec722d
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-07 15:43:32 +0800

    validates other form data from the request using zod

--- src/app/api/onboarding/[userId]/route.ts
+++ src/app/api/onboarding/[userId]/route.ts
@@ -1,8 +1,11 @@
 import { db } from "@/lib/prisma"
 import { NextResponse, NextRequest } from "next/server"
 import { utapi } from "@/utils/uploadthingapi"
+import { onboardingSchema } from "@/lib/validators/auth-validator"
+import { getServerSideSession } from "@/lib/auth"
 
 export const PATCH = async (request: NextRequest, { params }: { params: { userId: string } }) => {
+    const session = getServerSideSession()
     const body = await request.formData() 
     const userId = params.userId
 
@@ -12,8 +15,28 @@ export const PATCH = async (request: NextRequest, { params }: { params: { userId
     const facebookProfileLink = body.get("facebookLink")
     
     let uploadedImage = ""
+    
+    const parsedOnboardingData = onboardingSchema.safeParse({
+        githubLink: githubProfileLink,
+        facebookLink: facebookProfileLink,
+        name: name
+    })
 
     try {
+
+        if(!session) return NextResponse.json({
+            success: false,
+            message: "Unauthenticated, please log in first",
+        }, { status: 400 })   
+
+        if(!parsedOnboardingData.success) {
+            console.log(parsedOnboardingData.error)
+            return NextResponse.json({
+                errors: parsedOnboardingData.error.flatten().fieldErrors,
+                message: "Error in onboarding data.",
+            }, { status: 400 })   
+        }
+
         if (!userId) return NextResponse.json({
             message: "Missing params",
             success: false
@@ -28,24 +51,25 @@ export const PATCH = async (request: NextRequest, { params }: { params: { userId
         if(!existingUser) return NextResponse.json({
             message: "User not found",
             success: false
-        }, { status: 404})
+        }, { status: 404 })
 
         if(imageFile) {
             const response = await utapi.uploadFiles(imageFile)
             uploadedImage = response.data?.url as string
         }
 
+        //updates existing document, otherwise create a new one
         await db.social.upsert({
             where: {
                 userId: existingUser.id,
             },
             update: {
-                github: githubProfileLink as string,
-                facebook: facebookProfileLink as string,
+                github: parsedOnboardingData.data.githubLink,
+                facebook: parsedOnboardingData.data.facebookLink,
             },
             create: {
-                github: githubProfileLink as string,
-                facebook: facebookProfileLink as string,
+                github: parsedOnboardingData.data.githubLink,
+                facebook: parsedOnboardingData.data.facebookLink,
                 userId: existingUser.id,
             },
         });
@@ -65,8 +89,8 @@ export const PATCH = async (request: NextRequest, { params }: { params: { userId
                 name: true,
                 image: true,
                 onboarded: true,
-                social: true,
                 email: true,
+                social: true,
             },
         })
         
