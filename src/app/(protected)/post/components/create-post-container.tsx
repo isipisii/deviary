34242cc6a322bbce262d058d7bb340997b04ed36
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-09 17:04:26 +0800

    create a state and conditional rendered the forms

--- src/app/(protected)/post/components/create-post-container.tsx
+++ src/app/(protected)/post/components/create-post-container.tsx
@@ -1,49 +1,18 @@
 "use client"
 
-import { Tabs, Tab } from "@nextui-org/react";
-import { RiArticleFill } from "react-icons/ri";
-import { FaCode } from "react-icons/fa";
-import CreateBlogPostForm from "./create-blog-post-form";
-import CreateDiaryForm from "./create-diary-form";
+import { useState } from "react"
+import PostTypeTab from "./post-type-tab"
+import CreateBlogPostForm from "./create-blog-post-form"
+import CreateDiaryForm from "./create-diary-form"
 
 export default function CreatePostContainer() {
+  const [selectedTabItem, setSelectedTabItem] = useState("blog-post")
 
   return (
-    <div className="border border-borderColor">
-        <Tabs 
-            aria-label="Options" 
-            variant="underlined"
-            className="w-full"
-            classNames={{
-                tabList: "gap-6 w-full relative rounded-none p-0",
-                cursor: "w-1/2 bg-secondary h-[3px] rounded-full",
-                tab: "max-w-fit px-0 h-12",
-                tabContent: "group-data-[selected=true]:text-secondary"
-            }}
-        >
-            <Tab
-                key="blog-post"
-                title={
-                <div className="flex items-center space-x-2">
-                    <RiArticleFill />
-                    <span>Blog post</span>
-                </div>
-                }
-            >
-                <CreateBlogPostForm />
-            </Tab>
-            <Tab
-                key="diary"
-                title={
-                <div className="flex items-center space-x-2">
-                    <FaCode/>
-                    <span>Diary</span>
-                </div>
-                }
-            >
-                <CreateDiaryForm />
-            </Tab>
-        </Tabs>
+    <div className="space-y-6">
+        <PostTypeTab selectedTabItem={selectedTabItem} setSelectedTabItem={setSelectedTabItem} />
+        {selectedTabItem === "blog-post" && <CreateBlogPostForm />}
+        {selectedTabItem === "diary" && <CreateDiaryForm />}
     </div>
   )
 }
