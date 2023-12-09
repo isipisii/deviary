b018f3bbe4622efd0df4d6d0af6892b109f473c6
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-09 17:05:03 +0800

    remove max width

--- src/app/(protected)/post/create/page.tsx
+++ src/app/(protected)/post/create/page.tsx
@@ -4,7 +4,7 @@ export default function CreatePost() {
   // TODO
   return (
     <div className='p-6 md:p-12 w-full flex items-center justify-center'>
-      <div className="max-w-[900px] flex justify-between w-full flex-col gap-4">
+      <div className="flex justify-between w-full flex-col gap-4">
           <h2 className="font-semibold text-3xl" >Create a post</h2>
           <CreatePostContainer />
       </div> 
