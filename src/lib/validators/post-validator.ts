e75125c05d3c9c0c475606310c385863304361af
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-14 21:08:15 +0800

    create diary schema for validation

--- src/lib/validators/post-validator.ts
+++ src/lib/validators/post-validator.ts
@@ -1,9 +1,17 @@
 import z from "zod"
 
-export const postSchema = z.object({
+export const blogSchema = z.object({
     title: z.string()
     .min(1, { message: "Title is required" })
     .max(100, {message: "Title should not exceed to 100 characters"}),
-    tags: z.array(z.string()),
-    postBody: z.string().min(1, {message: "Body is required"})
+})
+
+export type TDiary = z.infer<typeof diarySchema>
+export const diarySchema = z.object({
+    title: z.string()
+    .min(1, { message: "Title is required" })
+    .max(100, { message: "Title should not exceed to 100 characters"}),
+    codeSnippet: z.string().min(1, { message: "Code snippet is required" }),
+    description: z.string().nullable(),
+    solution: z.string().min(1, { message: "Solution is required" })
 })
