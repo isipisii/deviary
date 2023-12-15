c702a8f38b02617fc26cccf5d1aa03bcd98df34b
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-15 20:53:22 +0800

    change api route

--- src/lib/services/post.api.ts
+++ src/lib/services/post.api.ts
@@ -1,7 +1,7 @@
-import { TBlogSchema } from "@/app/(protected)/post/components/create-blog-post-form";
+import { TBlogSchema } from "@/app/(protected)/post/components/blog-post-form";
 import axios from "axios";
 
 export async function createBlogPost(formData: FormData) {
-    const response = await axios.post("/api/post", formData)
+    const response = await axios.post("/api/blog", formData)
     return response.data.data as TBlogPost
 }
