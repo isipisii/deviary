52b3813590569df563db987f159baf1f56c4b64c
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-15 20:51:16 +0800

    create blog schema that will be use in server

--- src/lib/validators/post-validator.ts
+++ src/lib/validators/post-validator.ts
@@ -6,12 +6,20 @@ export const blogSchema = z.object({
     .max(100, {message: "Title should not exceed to 100 characters"}),
 })
 
+export const blogSchemaServer = z.object({
+    title: z.string()
+    .min(1, { message: "Title is required" })
+    .max(100, {message: "Title should not exceed to 100 characters"}),
+    content: z.string().min(1, { message: "Content is required" }),
+    tags: z.string().array().max(10, {message: "Tags should not be more than 10 items"}).nullable()
+}) 
+
 export type TDiary = z.infer<typeof diarySchema>
 export const diarySchema = z.object({
     title: z.string()
     .min(1, { message: "Title is required" })
     .max(100, { message: "Title should not exceed to 100 characters"}),
-    codeSnippet: z.string().min(1, { message: "Code snippet is required" }),
-    description: z.string().nullable(),
+    codeSnippet: z.string().nullable(),
+    description: z.string().min(1, { message: "Title is required" }),
     solution: z.string().min(1, { message: "Solution is required" })
 })
