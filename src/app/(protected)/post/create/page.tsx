1ae43b315ba6c06e45b180f01711638b51d3b2d2
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-17 15:10:39 +0800

    change width and position

--- src/app/(protected)/post/create/page.tsx
+++ src/app/(protected)/post/create/page.tsx
@@ -1,10 +1,10 @@
 import CreatePostContainer from "../components/create-post-container";
 
-export default function CreatePost() {
+export default async function CreatePost() {
   // TODO
   return (
     <div className='p-6 md:p-12 w-full flex items-center justify-center'>
-      <div className="flex justify-between w-full flex-col gap-4">
+      <div className="flex justify-between max-w-[900px] w-full flex-col item gap-4">
           <h2 className="font-semibold text-3xl" >Create a post</h2>
           <CreatePostContainer />
       </div> 
