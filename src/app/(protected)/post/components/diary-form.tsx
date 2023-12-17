7e554430616f3c83a33fe64f314e216b89d9fb0c
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-17 15:09:21 +0800

    create a mutation and add button disable and fields

--- src/app/(protected)/post/components/diary-form.tsx
+++ src/app/(protected)/post/components/diary-form.tsx
@@ -1,22 +1,29 @@
 "use client"
+
 import Tags from "@/components/shared/tags"
 import { Button, Input, Textarea } from "@nextui-org/react"
 import { useForm } from "react-hook-form"
 import { TDiarySchema } from "@/lib/validators/post-validator"
-import { AxiosError } from "axios";
 import { diarySchema } from "@/lib/validators/post-validator"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { useTags } from "@/lib/store/useTags"
+import { useCreateDiary } from "@/lib/services/post.api"
 
 export default function DiaryForm() {
+  const { tags } = useTags(state => state)
   const { watch, register, formState: { errors }, handleSubmit, reset } = useForm<TDiarySchema>({
     resolver: zodResolver(diarySchema)
   })
+  const { mutate: createDiaryMutation, isPending } = useCreateDiary(useForm())
+  const isButtonDisabled = !(!!watch("title") && !!watch("description") && !!watch("solution")) || isPending
 
   function handleCreateDiary(formValue: TDiarySchema) {
-    console.log(formValue)
+    const joinedTags = tags.join(",")
+    createDiaryMutation({...formValue, tags: joinedTags})
   }
+  
   return (
-    <div className="max-w-[700px] grid gap-4">
+    <div className="grid gap-4">
       <Tags />
       <form className="flex-col flex gap-4" onSubmit={handleSubmit(handleCreateDiary)}>
         <Input
@@ -24,14 +31,16 @@ export default function DiaryForm() {
           isRequired
           label="Title"
           radius="lg"
+          size="sm"
           variant="bordered"
           classNames={{
             label: "font-semibold", 
-            inputWrapper:" border-borderColor border-2 rounded-xl",
+            inputWrapper:"border-borderColor border-2 rounded-xl",
           }} 
           {...register("title")}
           errorMessage={errors.title?.message}
           isInvalid={!!errors.title}
+          isDisabled={isPending}
         />
         <Textarea
           labelPlacement="inside"
@@ -46,6 +55,7 @@ export default function DiaryForm() {
           {...register("codeSnippet")}
           errorMessage={errors.codeSnippet?.message}
           isInvalid={!!errors.codeSnippet}
+          isDisabled={isPending}
         />
         <Textarea
           labelPlacement="inside"
@@ -61,6 +71,7 @@ export default function DiaryForm() {
           {...register("description")}
           errorMessage={errors.description?.message}
           isInvalid={!!errors.description}
+          isDisabled={isPending}
         />
         <Textarea
           labelPlacement="inside"
@@ -76,13 +87,15 @@ export default function DiaryForm() {
           {...register("solution")}
           errorMessage={errors.solution?.message}
           isInvalid={!!errors.solution}
+          isDisabled={isPending}
         />
         <Button
           type="submit" 
-          size="lg"
+          size="md"
           color="secondary" 
           className="rounded-xl text-white max-w-[200px] w-full font-semibold self-end"
-          // isDisabled={isButtonDisabled}
+          isDisabled={isButtonDisabled}
+          isLoading={isPending}
         >
           Create
         </Button>
