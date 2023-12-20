0bfa04f272b7052506a3f4abd5c2b4e821a9c7c8
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-20 12:08:49 +0800

    add prefetching and hydration of feed posts using react-query

--- src/app/(protected)/feed/page.tsx
+++ src/app/(protected)/feed/page.tsx
@@ -2,8 +2,19 @@
 import { Button } from "@nextui-org/react";
 import { FiFilter } from "react-icons/fi";
 import FeedContainer from "./components/feed-container";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import { getFeedPosts } from "@/lib/services/post.api";
 
 export default async function Feed() {
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: ["posts"],
+    initialPageParam: "",
+    queryFn: ({ pageParam: lastCursor }) => getFeedPosts(5, lastCursor),
+    getNextPageParam: (lastPage: TFeedPostsPage) => lastPage.metaData ? lastPage?.metaData.lastCursor : null,
+  })
+
   return (
     <div className="p-12">
         <div className="flex items-center justify-between">
@@ -14,10 +25,12 @@ export default async function Feed() {
                 className="border-borderColor border-1 rounded-xl text-[1rem]" 
                 startContent={<FiFilter />}
             >
-                Filter
+              Filter
             </Button>
         </div>
-        <FeedContainer />
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <FeedContainer />
+        </HydrationBoundary>
     </div>
   )
 }
