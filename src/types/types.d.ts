f5a5c8b50c5748544e45a96689f5100e902dfb83
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-20 12:11:28 +0800

    add TFeedPostsPage and TPost types

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -11,14 +11,41 @@ type TUser = {
     image: string
 }
 
-type TBlogPost = {
+type TFeedPostsPage = {
+    metaData: {
+        hasNextPage: boolean
+        lastCursor: string
+    }
+    posts: TPost[] 
+}
+
+type TPost = {
+    type: PostType.BLOG_POST | PostType.CODE_DIARY
+    tags: string[]
+    id: string
+    blog?: TBlog 
+    diary?: TDiary
+    authorId: string
+    created_at: Date
+}
+
+type TBlog = {
     content: string
     thumbnail: {
         imageKey: string
         imageUrl: striong
     }
     title: string
-    authorId: string
-    tags: string
-    created_at: Date
 }
+
+type TDiary = {
+    title: string
+    codeSnippet?: string
+    description: string
+    solution: string
+}
+
+// enum PostType {
+//     CODE_DIARY = "CODE_DIARY",
+//     BLOG_POST = "BLOG_POST"
+// }
