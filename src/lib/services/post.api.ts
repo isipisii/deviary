edcd4a29ae6e82f7e93c6bd327f6c0e5574b3de0
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-20 12:12:10 +0800

    add useFeedPosts hook and update useCreateBlogPost onSuccess

--- src/lib/services/post.api.ts
+++ src/lib/services/post.api.ts
@@ -1,23 +1,37 @@
 import axios, { AxiosError } from "axios";
 import { TDiarySchema } from "../validators/post-validator";
-import { useMutation } from "@tanstack/react-query";
+import { useInfiniteQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next-nprogress-bar";
 import { toast } from "sonner";
 import { UseFormReturn } from "react-hook-form";
 
-// export async function createBlogPost(blogPostData: FormData) {
-//     const response = await axios.post("/api/blog", blogPostData)
-//     return response.data.data as TBlogPost
-// }
+
+export async function getFeedPosts(take: number, lastCursor: string) {
+    const response = await axios.get("/api/post", {
+        params: { take, lastCursor }
+    })  
+    return response.data.data as TFeedPostsPage
+}
+
+export function useFeedPosts() {
+    return useInfiniteQuery({
+      queryKey: ["posts"],
+      initialPageParam: "",
+      queryFn: ({ pageParam: lastCursor }) => getFeedPosts(5, lastCursor),
+      getNextPageParam: (lastPage) => lastPage.metaData ? lastPage?.metaData.lastCursor : null,
+  });
+}
 
 export function useCreateBlogPost(clearForm: () => void) {
+    const queryClient = useQueryClient()
     const router = useRouter()
     return useMutation({
         mutationFn: async (blogPostData: FormData) => {
             const response = await axios.post("/api/blog", blogPostData)
-            return response.data.data as TBlogPost
+            return response.data.data as TBlog
         },
         onSuccess: (data) => {
+            queryClient.invalidateQueries({ queryKey: ['posts'] })
             toast.success("Blog posted sucessfully")
             clearForm()
             router.push("/feed")
@@ -35,7 +49,7 @@ export function useCreateDiary(formReturn: UseFormReturn) {
             const response = await axios.post("/api/diary", diaryData)
             return response.data.data 
         },
-        onSuccess: (data) => {
+        onSuccess: () => {
             toast.success("Diary posted sucessfully")
             formReturn.reset()
             router.push("/feed")
