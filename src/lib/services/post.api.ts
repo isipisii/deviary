b85dac6a8e84f7f6f3fa387659b6e2066a771b22
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-22 11:11:23 +0800

    refactor useCreateBlogPost and useCreateDiary hooks

--- src/lib/services/post.api.ts
+++ src/lib/services/post.api.ts
@@ -1,61 +1,111 @@
 import axios, { AxiosError } from "axios";
 import { TDiarySchema } from "../validators/post-validator";
-import { useInfiniteQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  InfiniteData,
+  useInfiniteQuery,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
 import { useRouter } from "next-nprogress-bar";
 import { toast } from "sonner";
 import { UseFormReturn } from "react-hook-form";
 
-
 export async function getFeedPosts(take: number, lastCursor: string) {
-    const response = await axios.get("/api/post", {
-        params: { take, lastCursor }
-    })  
-    return response.data.data as TFeedPostsPage
+  const response = await axios.get("/api/post", {
+    params: { take, lastCursor },
+  });
+  return response.data.data as TFeedPostsPage;
 }
 
 export function useFeedPosts() {
-    return useInfiniteQuery({
-      queryKey: ["posts"],
-      initialPageParam: "",
-      queryFn: ({ pageParam: lastCursor }) => getFeedPosts(5, lastCursor),
-      getNextPageParam: (lastPage) => lastPage.metaData ? lastPage?.metaData.lastCursor : null,
+  return useInfiniteQuery({
+    queryKey: ["posts"],
+    initialPageParam: "",
+    queryFn: ({ pageParam: lastCursor }) => getFeedPosts(5, lastCursor),
+    getNextPageParam: (lastPage) =>
+      lastPage.metaData ? lastPage?.metaData.lastCursor : null,
   });
 }
 
 export function useCreateBlogPost(clearForm: () => void) {
-    const queryClient = useQueryClient()
-    const router = useRouter()
-    return useMutation({
-        mutationFn: async (blogPostData: FormData) => {
-            const response = await axios.post("/api/blog", blogPostData)
-            return response.data.data as TBlog
-        },
-        onSuccess: (data) => {
-            queryClient.invalidateQueries({ queryKey: ['posts'] })
-            toast.success("Blog posted sucessfully")
-            clearForm()
-            router.push("/feed")
-        },
-        onError: (error: AxiosError<ErrorResponse>) =>  {
-            toast.error(error.response?.data?.message)
-        },
-    })
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: async (blogPostData: FormData) => {
+      const response = await axios.post("/api/blog", blogPostData);
+      return response.data.data as TPost;
+    },
+    onSuccess: (data) => {
+      queryClient.setQueryData<InfiniteData<TFeedPostsPage>>(
+        ["posts"],
+        (oldData) => {
+          const newData = oldData
+            ? {
+                ...oldData,
+                pages: oldData.pages.map((page, index) => {
+                  if (index === 0) {
+                    return {
+                      ...page,
+                      posts: [data as TPost, ...page.posts],
+                    };
+                  }
+                  return page;
+                }),
+              }
+            : undefined;
+          return newData;
+        }
+      );
+      toast.success("Blog posted sucessfully");
+      clearForm();
+      router.push("/feed");
+    },
+    onError: (error: AxiosError<ErrorResponse>) => {
+      toast.error(error.response?.data?.message);
+    },
+  });
 }
 
 export function useCreateDiary(formReturn: UseFormReturn) {
-    const router = useRouter()
-    return useMutation({
-        mutationFn: async (diaryData: TDiarySchema & { tags: string }) => {
-            const response = await axios.post("/api/diary", diaryData)
-            return response.data.data 
-        },
-        onSuccess: () => {
-            toast.success("Diary posted sucessfully")
-            formReturn.reset()
-            router.push("/feed")
-        },
-        onError: (error: AxiosError<ErrorResponse>) =>  {
-            toast.error(error.response?.data?.message)
-        },
-    })
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (diaryData: TDiarySchema & { tags: string }) => {
+      const response = await axios.post("/api/diary", diaryData);
+      return response.data.data;
+    },
+    onSuccess: (data) => {
+      //for optimistic update of the ui
+      queryClient.setQueryData<InfiniteData<TFeedPostsPage>>(
+        ["posts"],
+        (oldData) => {
+          const newData = oldData
+            ? {
+                ...oldData,
+                pages: oldData.pages.map((page, index) => {
+                  if (index === 0) {
+                    return {
+                      ...page,
+                      posts: [data as TPost, ...page.posts],
+                    };
+                  }
+                  return page;
+                }),
+              }
+            : oldData;
+          return newData;
+        }
+      );
+
+      // await queryClient.invalidateQueries({ queryKey: ["posts"] });
+      toast.success("Diary posted sucessfully");
+      formReturn.reset();
+      router.push("/feed");
+    },
+    onError: (error: AxiosError<ErrorResponse>) => {
+      toast.error(error.response?.data?.message);
+    },
+  });
 }
