dc526909db223eecfdf0b5a0f11f2e98f6a1fe2a
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-27 12:59:56 +0800

    fix styling issue in CreatePost component

--- src/app/(protected)/post/create/page.tsx
+++ src/app/(protected)/post/create/page.tsx
@@ -4,7 +4,7 @@ export default async function CreatePost() {
   // TODO
   return (
     <div className='p-6 md:p-12 w-full flex items-center justify-center'>
-      <div className="flex justify-between max-w-[900px] w-full flex-col item gap-4">
+      <div className="flex justify-between max-w-[900px] w-full flex-col gap-4">
           <h2 className="font-semibold text-3xl" >Create a post</h2>
           <CreatePostContainer />
       </div> 
