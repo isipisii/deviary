fb23e9c8496b33c40a11ae078c0f609f13314688
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-31 14:29:54 +0800

    Refactor feed page component

--- src/app/(protected)/feed/page.tsx
+++ src/app/(protected)/feed/page.tsx
@@ -3,21 +3,23 @@ import FeedContainer from "./components/feed-container";
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import { getFeedPosts } from "@/lib/services/post.api";
 import FilterDropdown from "@/components/ui/filter-dropdown";
+import { QueryKeys } from "@/lib/constants";
+import PageTitle from "@/components/ui/page-title";
 
 export default async function Feed() {
   const queryClient = new QueryClient()
 
   await queryClient.prefetchInfiniteQuery({
-    queryKey: ["posts"],
+    queryKey: [QueryKeys.Posts],
     initialPageParam: "",
     queryFn: ({ pageParam: lastCursor }) => getFeedPosts(5, lastCursor),
-    getNextPageParam: (lastPage: TFeedPostsPage) => lastPage.metaData ? lastPage?.metaData.lastCursor : null,
+    getNextPageParam: (lastPage: TPage<TPost[]>) => lastPage.metaData ? lastPage?.metaData.lastCursor : null,
   })
 
   return (
     <div className="p-12">
         <div className="flex items-center justify-between">
-            <h2 className="font-semibold text-3xl">Feed</h2>
+            <PageTitle>Feed</PageTitle>
             <FilterDropdown />
         </div>
         <HydrationBoundary state={dehydrate(queryClient)}>
