fe8e655554003bb52eb2c19079af91b54d7c1fa4
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-31 14:29:19 +0800

    Refactor create post page to use PageTitle component

--- src/app/(protected)/post/create/page.tsx
+++ src/app/(protected)/post/create/page.tsx
@@ -1,11 +1,12 @@
 import CreatePostContainer from "../components/create-post-container";
+import PageTitle from "@/components/ui/page-title";
 
 export default async function CreatePost() {
   // TODO
   return (
     <div className='p-6 md:p-12 w-full flex items-center justify-center'>
       <div className="flex justify-between max-w-[900px] w-full flex-col gap-4">
-          <h2 className="font-semibold text-3xl" >Create a post</h2>
+          <PageTitle>Create a post</PageTitle>
           <CreatePostContainer />
       </div> 
     </div>
