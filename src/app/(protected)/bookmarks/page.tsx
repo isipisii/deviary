1acd29ec1b49b5ba249dfd0317cb9e0cad2c62db
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-31 14:15:09 +0800

    add bookmarks page component

---
+++ src/app/(protected)/bookmarks/page.tsx
@@ -0,0 +1,29 @@
+import PageTitle from "@/components/ui/page-title";
+import { getBookmarks } from "@/lib/services/bookmark.api";
+import { QueryKeys } from "@/lib/constants";
+import BookmarksContainer from "./components/bookmarks-container";
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
+
+export default async function BookMarkpage() {
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: [QueryKeys.Bookmarks],
+    initialPageParam: "",
+    queryFn: ({ pageParam: lastCursor }) => getBookmarks(5, lastCursor),
+    getNextPageParam: (lastPage: TPage<TBookmark[]>) => lastPage.metaData ? lastPage?.metaData.lastCursor : null,
+  })
+
+  return (
+    <div className="p-12">
+      <PageTitle>Bookmarks</PageTitle>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+          <BookmarksContainer />
+      </HydrationBoundary>
+    </div>
+  )
+}
