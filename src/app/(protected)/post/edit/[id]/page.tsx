8e49b6a5925438bcd8735e2cf713c3c8f8572329
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-12-31 14:29:09 +0800

    Add noStore() to remove caching in EditPost

--- src/app/(protected)/post/edit/[id]/page.tsx
+++ src/app/(protected)/post/edit/[id]/page.tsx
@@ -1,9 +1,14 @@
 import { db } from "@/lib/prisma";
 import BlogPostForm from "../../components/blog-post-form";
 import DiaryForm from "../../components/diary-form";
+import PageTitle from "@/components/ui/page-title";
+import { unstable_noStore as noStore } from "next/cache";
 
 export default async function EditPost({ params }: { params: { id: string } }) {
-  const postToEdit = (await db.post.findUnique({
+  //to remove caching
+  noStore();
+
+  const postToEdit = await db.post.findUnique({
     where: {
       id: params.id,
     },
@@ -11,17 +16,17 @@ export default async function EditPost({ params }: { params: { id: string } }) {
       blog: true,
       diary: true,
     },
-  })) as TPost;
+  }) as TPost;
 
   return (
     <div className="p-6 md:p-12 w-full flex items-center justify-center">
       <div className="flex justify-between max-w-[900px] w-full flex-col item gap-6">
-        <h2 className="font-semibold text-3xl">
-          Edit {" "}
+        <PageTitle>
+          Edit{" "}
           <span className="text-secondary">
             {postToEdit?.type === "BLOG_POST" ? "blog" : "diary"}
           </span>
-        </h2>
+        </PageTitle>
         {postToEdit?.type === "BLOG_POST" ? (
           <BlogPostForm postToEdit={postToEdit} />
         ) : (
