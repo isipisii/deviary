c3ca90e023dfd4f1982b1cd98e7b73f6217fdcac
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-02 20:16:10 +0800

    create custom hook for detecting the specified viewport

---
+++ src/lib/hooks/useDetectViewport.ts
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const SCREEN_SIZES: Record<string, number> = {
+    "xs": 375,
+    "sm": 576,
+    "md": 768,
+    "lg": 992,
+    "xl": 1200,
+};
+
+type TScreenSize = "xs" | "sm" | "md" | "lg" | "xl"
+
+function getWidth() {
+    //to avoid runtime error in server 
+    if(typeof window !== "undefined" && typeof document !== "undefined") {
+        return Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    }
+
+    return 0
+};
+
+export default function useDetectViewport(screenSize: TScreenSize) {
+    const [currentScreenWidth, setCurrentScreenWidth] =  useState(getWidth)
+    const isInViewport = currentScreenWidth <= SCREEN_SIZES[screenSize] 
+
+    useEffect(() => {
+        const handleResize = () => {
+            setCurrentScreenWidth(getWidth);
+        };
+
+        window.addEventListener("resize", handleResize)
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    },[])
+
+    return { isInViewport, currentScreenWidth }
+} 
