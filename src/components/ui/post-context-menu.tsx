f56752fd41c00a1b6ecb2641c7619bfab75db6d1
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-03 16:41:13 +0800

    add delete modal and bookmark functionality to PostContextMenu component

--- src/components/ui/post-context-menu.tsx
+++ src/components/ui/post-context-menu.tsx
@@ -8,9 +8,13 @@ import {
   } from "@nextui-org/react";
 import { GoKebabHorizontal } from "react-icons/go";
 import { cn } from "@/utils/cn";
-import { useSession } from "next-auth/react";
 import { postContextMenuItems } from "@/lib/constants";
 import { useDeletePost } from "@/lib/services/post.api";
+import { useCreateBookmark, useRemoveBookmark } from "@/lib/services/bookmark.api";
+import { useRouter } from "next-nprogress-bar";
+import { useDisclosure } from "@nextui-org/react";
+import DeletePostModal from "./delete-modal";
+import { useSession } from "next-auth/react";
 
 interface IPostContextMenu {
     postType?: "BLOG_POST" | "CODE_DIARY"
@@ -19,41 +23,118 @@ interface IPostContextMenu {
 }
 
 export default function PostContextMenu({ post, postType, className }: IPostContextMenu) {
-  const { data } = useSession()
+  const { data, update} = useSession()
+  const bookmarks = data?.user.bookmarks as { id: string, postId: string;}[]
+  const router = useRouter()
   const items = data?.user.id === post.authorId ? postContextMenuItems : postContextMenuItems.filter(item => item.key !== "edit" && item.key !== "delete" )
-  const { mutate: deletePostMutation, isPending } = useDeletePost()
-    console.log(isPending)
-    
+  //for delete modal
+  const {isOpen, onOpen, onOpenChange, onClose} = useDisclosure();
+  const { mutate: deletePostMutation, isPending } = useDeletePost(onClose)
+  const { 
+    mutate: createBookmarkMutation, 
+    isPending: isAddingBookmark 
+  } = useCreateBookmark()
+  const { 
+    mutate: removeBookmarkMutation, 
+    isPending: isRemovingBookmark 
+  } = useRemoveBookmark()
+
+  function handleDeletePost(){
+    deletePostMutation(post.id)
+  }
+
+  function handleToggleBookmark() {
+    if(post.isBookmarked) {
+        if(post.bookmarkId) removeBookmarkMutation({bookmarkId: post.bookmarkId, postId: post.id})
+    } else {
+        createBookmarkMutation(post.id)
+    }
+  } 
+
   return (
-    <Dropdown 
-        className={cn(
-            "bg-cardBg",
-            className 
-        )} 
-        placement="bottom-end"
-    >
-        <DropdownTrigger>
-            <Button
-                variant="light"
-                size="sm"
-                isIconOnly
-                // className="rounded-lg text-[1.2rem]"
-                className={cn("rounded-lg text-[1.2rem]", {"text-white" : postType === "BLOG_POST"})}
-                startContent={<GoKebabHorizontal />}
-            />
-        </DropdownTrigger>
-        <DropdownMenu aria-label="Dynamic Actions" items={items} variant="flat">
-            {(item => (
-                <DropdownItem 
-                    key={item.key}
-                    color={item.key === "delete" ? "danger" : undefined}
-                    className={item.key === "delete" ? "text-danger" : ""}
-                    onClick={() => deletePostMutation(post.id)}
-                >
-                    {item.label}
-                </DropdownItem>
-            ))}
-        </DropdownMenu>
-    </Dropdown>
+    <>  
+        <DeletePostModal
+            isOpen={isOpen} 
+            onOpenChange={onOpenChange} 
+            handleDelete={handleDeletePost}
+            isDeleting={isPending}
+        />
+        <Dropdown 
+            className={cn(
+                "bg-cardBg rounded-xl",
+                className 
+            )} 
+            placement="bottom-end"
+        >
+            <DropdownTrigger onClick={(e) => e.preventDefault()}>
+                <Button
+                    variant="light"
+                    size="sm"
+                    isIconOnly
+                    className={cn("rounded-lg text-[1.2rem]", {"text-white" : postType === "BLOG_POST"})}
+                    startContent={<GoKebabHorizontal />}
+                />
+            </DropdownTrigger>
+            <DropdownMenu 
+                aria-label="Dynamic Actions" 
+                items={items} 
+                variant="flat" 
+                // disabledKeys={isAddingBookmark || isRemovingBookmark ? ["bookmark"] : undefined}
+            >
+                {((item) => {
+                    if(item.key === "edit") {
+                        return (
+                            <DropdownItem 
+                                key={item.key}
+                                onClick={() => router.push(`/post/edit/${post.id}`)}
+                                startContent={<item.icon />}
+                                className="rounded-lg"
+                            >
+                                {item.label}
+                            </DropdownItem>
+                        )
+                    }
+
+                    if(item.key === "delete") {
+                        return ( 
+                            <DropdownItem 
+                                key={item.key}
+                                color="danger"
+                                className="text-danger rounded-lg"
+                                startContent={<item.icon />}
+                                onPress={onOpen}
+                            >
+                                {item.label}
+                            </DropdownItem>
+                        )
+                    }
+
+                    if(item.key === "bookmark") {
+                        return ( 
+                            <DropdownItem 
+                                key={item.key}
+                                color="warning"
+                                className={`rounded-lg ${post.isBookmarked ? "text-warning" : null}`}
+                                startContent={post.isBookmarked ? <item.activeIcon /> : <item.icon />}
+                                onClick={handleToggleBookmark}
+                            >
+                                {post.isBookmarked ? "Bookmarked" : item.label}
+                            </DropdownItem>
+                        )
+                    }
+
+                    return (
+                        <DropdownItem 
+                            key={item.key}
+                            startContent={<item.icon />}
+                            className="rounded-lg"
+                        >
+                            {item.label}
+                        </DropdownItem>
+                    )
+                })}
+            </DropdownMenu>
+        </Dropdown>
+    </>
   )
 }
