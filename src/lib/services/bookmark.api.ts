6bc47e60901867c01643e1fe4d3c1d56dffbad55
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-03 16:40:43 +0800

    fix optimistic update adding and removing a bookmark

--- src/lib/services/bookmark.api.ts
+++ src/lib/services/bookmark.api.ts
@@ -1,8 +1,7 @@
-import { useMutation, useInfiniteQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useInfiniteQuery, useQueryClient, InfiniteData } from "@tanstack/react-query";
 import axios, { AxiosError } from "axios";
 import { toast } from "sonner";
 import { QueryKeys } from "../constants";
-import { Session } from "next-auth";
 
 export async function getBookmarks(take: number, lastCursor: string) {
     const response = await axios.get("/api/bookmark", {
@@ -21,10 +20,7 @@ export function useGetBookmarks() {
     });
 }
 
-export type TBookmarks = { id: string, postId: string }[]
-type TUpdateSessionAndToken = (data?: any) => Promise<Session | null>
-
-export function useCreateBookmark(bookmarks: TBookmarks, updateSessionAndToken: TUpdateSessionAndToken) {
+export function useCreateBookmark() {
     const queryClient = useQueryClient()
 
     return useMutation({
@@ -32,37 +28,100 @@ export function useCreateBookmark(bookmarks: TBookmarks, updateSessionAndToken: 
             const response = await axios.post(`/api/bookmark/create/${postId}`)
             return response.data.data as TBookmark
         },
-        onSuccess: async (data) => {
-            await updateSessionAndToken({ bookmarks: [...bookmarks, { id: data.id, postId: data.postId }]})
-            await queryClient.invalidateQueries({queryKey: [QueryKeys.Bookmarks]})
+        onMutate: async (postId) => {
+            await queryClient.cancelQueries({ queryKey: [QueryKeys.Bookmarks] })
+            await queryClient.cancelQueries({ queryKey: [QueryKeys.Posts] })
+            const previousPosts =  queryClient.getQueryData([QueryKeys.Posts]) 
+
+            queryClient.setQueryData<InfiniteData<TPage<TPost[]>>>(
+                [QueryKeys.Posts],
+                (oldData) => {
+                  const newData = oldData
+                    ? {
+                        ...oldData,
+                        pages: oldData.pages.map((page) => {
+                          if (page) {
+                            return {
+                              ...page,
+                              data: page.data ? page.data.map(data => data.id === postId ? {...data, isBookmarked: true}: data) : []
+                            };
+                          }
+                          return page;
+                        }),
+                      }
+                    : oldData;
+    
+                  return newData;
+                }
+            );
             toast.success("Added to bookmarks")
+            return { previousPosts }
+        },
+        onSuccess: async () => {
+            return await queryClient.invalidateQueries({queryKey: [QueryKeys.Bookmarks]})
         },
         onError: (error: AxiosError<ErrorResponse>) => {
-            toast.error(error.response?.data.message)
+            toast.error("An error occured while adding to bookmark")
         },
-        // onSettled: () => {
-        //     console.log(data?.user)
-        // }
+        onSettled: async () => {
+           await queryClient.invalidateQueries({ queryKey: [QueryKeys.Posts, QueryKeys.Bookmarks] })
+        },
     })
 }
 
-export function useRemoveBookmark(bookmarks: TBookmarks, updateSessionAndToken: TUpdateSessionAndToken) {
+export function useRemoveBookmark() {
     const queryClient = useQueryClient()
     
     return useMutation({
-        mutationFn: async (bookmarkId: string) => {
+        mutationFn: async ({ bookmarkId, postId }: { bookmarkId: string, postId: string }) => {
             const response = await axios.delete(`/api/bookmark/delete/${bookmarkId}`)
             return response.data.data  
         },
+        onMutate: async ({ postId }) => {
+            await queryClient.cancelQueries({ queryKey: [QueryKeys.Bookmarks] })
+            await queryClient.cancelQueries({ queryKey: [QueryKeys.Posts] })
+            const previousPosts =  queryClient.getQueryData<InfiniteData<TPage<TPost[]>>>([QueryKeys.Posts]) 
+            const previousBookmarks =  queryClient.getQueryData<InfiniteData<TPage<TBookmark[]>>>([QueryKeys.Bookmarks]) 
+
+            queryClient.setQueryData<InfiniteData<TPage<TPost[]>>>(
+                [QueryKeys.Posts],
+                (oldData) => {
+                  const newData = oldData
+                    ? {
+                        ...oldData,
+                        pages: oldData.pages.map((page) => {
+                          if (page) {
+                            return {
+                              ...page,
+                              data: page.data ? page.data.map(data => data.id === postId ? {...data, isBookmarked: false}: data) : []
+                            };
+                          }
+                          return page;
+                        }),
+                      }
+                    : oldData;
+                  return newData;
+                }
+            );
+            toast.success("Removed from bookmarks")
+            return { previousPosts, previousBookmarks }
+        },
         onSuccess: async () => {
-            await queryClient.invalidateQueries({queryKey: [QueryKeys.Bookmarks]})
+            return await queryClient.invalidateQueries({queryKey: [QueryKeys.Bookmarks]})
         },
-        onMutate: async (bookmarkId) => {
-            const updatedBookmarks = bookmarks.filter(bookmark => bookmark?.id !== bookmarkId)
-            await updateSessionAndToken({ bookmarks: updatedBookmarks })
+        onError: (error, bookmark, context) => {
+            // queryClient.setQueryData(
+            //     [QueryKeys.Posts],
+            //     context?.previousPosts
+            // )
+            // queryClient.setQueryData(
+            //     [QueryKeys.Bookmarks],
+            //     context?.previousBookmarks
+            // )
+            toast.error("An error occured while removing from bookmark")
         },
-        onError: (error: AxiosError<ErrorResponse>) => {
-            toast.error(error.response?.data.message)
-        }
+        onSettled: () => {
+            queryClient.invalidateQueries({ queryKey: [QueryKeys.Posts, QueryKeys.Bookmarks] })
+        },
     })
 }
