3b9de13d3971274c669242261b9c236a272c8665
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-04 15:49:50 +0800

    refactor types in types.d.ts file

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,58 +1,57 @@
-//this is for axios' generic type response 
+//this is for axios' generic type response
 interface ErrorResponse {
-    message: string
+  message: string;
 }
 
 type TUser = {
-    id: readonly string 
-    email: string
-    onboarded?: boolean
-    name: string
-    image: string
-}
+  id: readonly string;
+  email: string;
+  onboarded?: boolean;
+  name: string;
+  image: string;
+};
 
-type TPage <TData> = {
-    metaData: {
-        hasNextPage: boolean
-        lastCursor: string
-    }
-    data: TData
-}
+type TPage<TData> = {
+  metaData: {
+    hasNextPage: boolean;
+    lastCursor: string;
+  };
+  data: TData;
+};
 
 type TPost = {
-    type: "BLOG_POST" | "CODE_DIARY" 
-    tags: string[]    
-    id: readonly string 
-    author?: TUser
-    blog?: TBlog 
-    diary?: TDiary
-    isBookmarked: boolean
-    bookmarkId?: readonly string 
-    authorId?:  readonly string
-    createdAt: Date
-}
+  type: "BLOG_POST" | "CODE_DIARY";
+  tags: string[];
+  id: readonly string;
+  author?: TUser;
+  blog?: TBlog;
+  diary?: TDiary;
+  isBookmarked?: boolean;
+  bookmarkId?: readonly string;
+  authorId?: readonly string;
+  createdAt: Date;
+};
 
 type TBlog = {
-    content: string
-    thumbnail?: {
-        imageKey?: string
-        imageUrl?: string
-    }
-    title: string
-}
+  content: string;
+  thumbnail?: {
+    imageKey?: string;
+    imageUrl?: string;
+  };
+  title: string;
+};
 
 type TBookmark = {
-    id: readonly string
-    postId: string
-    post: TPost
-    userId: string
-    user: TUser
-}
+  id: readonly string;
+  postId: string;
+  post: TPost;
+  userId: string;
+  user: TUser;
+};
 
 type TDiary = {
-    title: string
-    codeSnippet?: string
-    description: string
-    solution: string
-}
-
+  title: string;
+  codeSnippet?: string;
+  description: string;
+  solution: string;
+};
