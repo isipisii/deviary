5befa81985306ebe53fd1de2eb3cdb63d4e4e540
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-04 15:52:48 +0800

    change card container's positioning to grid

--- src/components/layout/card-container.tsx
+++ src/components/layout/card-container.tsx
@@ -1,9 +1,19 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
+import { useSideBarNavStore } from "@/lib/store/useSideBarNavStore";
+import { cn } from "@/utils/cn";
 
 export default function CardContainer({ children }: { children: ReactNode }) {
+  const { isSideBarMinimized } = useSideBarNavStore((state) => state);
   return (
-    <div className="flex gap-12 items-center justify-center flex-wrap mt-8 w-full">
-        {children}
+    // <div className="flex-start mt-8 flex flex-wrap items-center justify-center gap-6">
+    <div
+      className={`mt-8 grid w-full grid-cols-1 place-items-center gap-8 ${
+        !isSideBarMinimized
+          ? "md:grid-cols-1 lg:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4"
+          : "md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5"
+      } `}
+    >
+      {children}
     </div>
-  )
+  );
 }
