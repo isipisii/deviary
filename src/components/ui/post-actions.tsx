7fb732a2d3e7e854440173c8a52d5c69e2ca32fc
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-04 15:55:46 +0800

    refactor PostActions component

--- src/components/ui/post-actions.tsx
+++ src/components/ui/post-actions.tsx
@@ -1,73 +1,76 @@
-import { Tooltip, Button } from '@nextui-org/react'
-import React from 'react'
-import { FaRegComments } from 'react-icons/fa'
-import { TbArrowBigUp, TbShare3 } from 'react-icons/tb'
+import { Tooltip, Button } from "@nextui-org/react";
+import { useRouter } from "next-nprogress-bar";
+import { FaRegComments } from "react-icons/fa";
+import { TbArrowBigUp, TbShare3 } from "react-icons/tb";
 
-export default function PostActions() {
+export default function PostActions({ postId }: { postId: string }) {
+  const router = useRouter();
+
   return (
     <div className="flex items-center justify-between">
-        <Tooltip
-            placement="bottom" 
-            content="Upvote" 
-            className="bg-background z-10" 
-            showArrow  
-            classNames={{
-                base: [
-                // arrow color
-                "before:bg-background dark:before:bg-background",
-                ],
-            }}
-        >
-            <Button
-                isIconOnly 
-                className="text-2xl rounded-xl text-[#A1A1AA] bg-[#fff0] 
+      <Tooltip
+        placement="bottom"
+        content="Upvote"
+        className="z-10 bg-background"
+        showArrow
+        classNames={{
+          base: [
+            // arrow color
+            "before:bg-background dark:before:bg-background",
+          ],
+        }}
+      >
+        <Button
+          isIconOnly
+          className="rounded-xl bg-[#fff0] text-2xl text-[#A1A1AA] 
                 hover:bg-[#34b60058] hover:text-[#34FF00]"
-            >
-                <TbArrowBigUp /> 
-            </Button>
-        </Tooltip>
-
-        <Tooltip 
-            placement="bottom" 
-            content="Comments" 
-            className="bg-background z-10" 
-            showArrow 
-            classNames={{
-                base: [
-                // arrow color
-                "before:bg-background dark:before:bg-background",
-                ],
-            }}
         >
-            <Button 
-                isIconOnly 
-                className="text-2xl rounded-xl text-[#A1A1AA] bg-[#fff0] 
+          <TbArrowBigUp />
+        </Button>
+      </Tooltip>
+
+      <Tooltip
+        placement="bottom"
+        content="Comments"
+        className="z-10 bg-background"
+        showArrow
+        classNames={{
+          base: [
+            // arrow color
+            "before:bg-background dark:before:bg-background",
+          ],
+        }}
+      >
+        <Button
+          isIconOnly
+          className="rounded-xl bg-[#fff0] text-2xl text-[#A1A1AA] 
                 hover:bg-[#003db647] hover:text-[#639cff]"
-            >
-                <FaRegComments />
-            </Button>
-        </Tooltip>
-
-        <Tooltip 
-            placement="bottom" 
-            content="Share" 
-            className="bg-background z-10" 
-            showArrow 
-            classNames={{
-                base: [
-                // arrow color
-                "before:bg-background dark:before:bg-background",
-                ],
-            }}
+          onClick={() => router.push(`/post/${postId}`)}
         >
-            <Button 
-                isIconOnly 
-                className="text-2xl rounded-xl text-[#A1A1AA] bg-[#fff0] 
+          <FaRegComments />
+        </Button>
+      </Tooltip>
+
+      <Tooltip
+        placement="bottom"
+        content="Share"
+        className="z-10 bg-background"
+        showArrow
+        classNames={{
+          base: [
+            // arrow color
+            "before:bg-background dark:before:bg-background",
+          ],
+        }}
+      >
+        <Button
+          isIconOnly
+          className="rounded-xl bg-[#fff0] text-2xl text-[#A1A1AA] 
                 hover:bg-[#dd0dba3c] hover:text-[#DD0DB9]"
-            >
-                <TbShare3 />
-            </Button>
-        </Tooltip>
-    </div>   
-  )
-}   
+        >
+          <TbShare3 />
+        </Button>
+      </Tooltip>
+    </div>
+  );
+}
