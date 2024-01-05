648402341ed32b406e965984ad024c1b6a46181f
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-05 11:27:52 +0800

    include post

--- src/app/api/bookmark/create/[postId]/route.ts
+++ src/app/api/bookmark/create/[postId]/route.ts
@@ -3,55 +3,82 @@ import { NextResponse, NextRequest } from "next/server";
 import { getServerSideSession } from "@/lib/auth";
 
 type TParams = {
-    params: {
-        postId: string
-    }
-}
+  params: {
+    postId: string;
+  };
+};
 
 export const POST = async (request: NextRequest, { params }: TParams) => {
-    const session = await getServerSideSession()
-    const postId = params.postId
-    
-    try {
-        if(!session){
-            return NextResponse.json({
-                message: "Unauthenticated, please log in first",
-                success: false
-            }, { status: 400 })
-        }
+  const session = await getServerSideSession();
+  const postId = params.postId;
 
-        //checks if the post has been bookmarked by the user
-        const existingBookmark = await db.bookmark.findFirst({
-            where: {
-                postId,
-                userId: session?.user.id as string
-            }
-        })
-
-        if(existingBookmark){
-            return NextResponse.json({
-                message: "Post has already been bookmarked",
-                success: false
-            }, { status: 409 })
-        }
-
-        const createdBookmark = await db.bookmark.create({
-            data: {
-                postId,
-                userId: session?.user.id as string
-            },
-        })
-
-        return NextResponse.json({
-            data: createdBookmark,
-            message: "Bookmark created",
-            success: true
-        }, { status: 200 })
-
-    } catch (error) {
-        return NextResponse.json({
-            message: "Internal Server Error",
-            success: false
-        }, { status: 500 })
+  try {
+    if (!session) {
+      return NextResponse.json(
+        {
+          message: "Unauthenticated, please log in first",
+          success: false,
+        },
+        { status: 400 },
+      );
     }
-}
+
+    //checks if the post has been bookmarked by the user
+    const existingBookmark = await db.bookmark.findFirst({
+      where: {
+        postId,
+        userId: session?.user.id as string,
+      },
+    });
+
+    if (existingBookmark) {
+      return NextResponse.json(
+        {
+          message: "Post has already been bookmarked",
+          success: false,
+        },
+        { status: 409 },
+      );
+    }
+
+    const createdBookmark = await db.bookmark.create({
+      data: {
+        postId,
+        userId: session?.user.id as string,
+      },
+      include: {
+        post: {
+          include: {
+            blog: true,
+            diary: true,
+            author: {
+              select: {
+                id: true,
+                name: true,
+                email: true,
+                image: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(
+      {
+        data: createdBookmark,
+        message: "Bookmark created",
+        success: true,
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Internal Server Error",
+        success: false,
+      },
+      { status: 500 },
+    );
+  }
+};
