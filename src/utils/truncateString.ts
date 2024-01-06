12708c92baf23914adcd410f92f2f6bc4d869a86
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-06 20:40:01 +0800

    Delete truncateString utility function

--- src/utils/truncateString.ts
+++
@@ -1,5 +0,0 @@
-export default function truncateString(text: string, maximumCharacters: number): string {
-    if(text.length > maximumCharacters) {
-        return text.slice(0, maximumCharacters) + "..."
-    } return text
-}
