d31cf8a7ea10681d6220b566f2b0daf0baff5e10
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-08 20:46:26 +0800

    adjust columns

--- src/components/layout/card-container.tsx
+++ src/components/layout/card-container.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from "react";
 import { useSideBarNavStore } from "@/lib/store/useSideBarNavStore";
-import { cn } from "@/utils/cn";
 
 export default function CardContainer({ children }: { children: ReactNode }) {
   const { isSideBarMinimized } = useSideBarNavStore((state) => state);
@@ -8,9 +7,9 @@ export default function CardContainer({ children }: { children: ReactNode }) {
     // <div className="flex-start mt-8 flex flex-wrap items-center justify-center gap-6">
     <div
       className={`mt-8 grid w-full grid-cols-1 place-items-center gap-8 ${
-        !isSideBarMinimized
-          ? "md:grid-cols-1 lg:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4"
-          : "md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5"
+        isSideBarMinimized
+          ? "md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5"
+          : "md:grid-cols-1 lg:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4"
       } `}
     >
       {children}
