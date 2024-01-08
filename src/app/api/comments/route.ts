6cc15c3ce3fae38749d01eaa0d1aab75538bb7fd
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-08 21:11:56 +0800

    change search params instead of dynamic route in GET and POST

---
+++ src/app/api/comments/route.ts
@@ -0,0 +1,161 @@
+import { db } from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
+import { getServerSideSession } from "@/lib/auth";
+
+export const GET = async (request: NextRequest) => {
+  const url = new URL(request.url);
+  const postId = url.searchParams.get("postId") as string;
+  const take = Number(url.searchParams.get("take"));
+  const lastCursor = url.searchParams.get("lastCursor") as string;
+
+  try {
+    if(!postId) {
+      return NextResponse.json(
+        {
+          message: "Missing post id",
+          successs: false,
+        },
+        { status: 400 },
+      );
+    }
+
+    const comments = await db.comment.findMany({
+      where: {
+        postId,
+      },
+
+      take: take ?? 5,
+      ...(lastCursor && {
+        cursor: {
+          id: lastCursor,
+        },
+        skip: 1,
+      }),
+
+      include: {
+        user: {
+          select: {
+            name: true,
+            email: true,
+            image: true,
+          },
+        },
+      },
+    });
+
+    if (comments.length === 0) {
+      return NextResponse.json(
+        {
+          data: [],
+          metaData: {
+            lastCursor: null,
+            hasNextPage: false,
+          },
+        },
+        { status: 200 },
+      );
+    }
+
+    const lastComment = comments.at(-1);
+    const cursor = lastComment?.id;
+
+    const nextPage = await db.comment.findMany({
+      take: take ?? 5,
+      skip: 1,
+      cursor: {
+        id: cursor,
+      },
+    });
+
+    const data = {
+      data: comments,
+      metaData: {
+        lastCursor: cursor,
+        hasNextPage: nextPage.length > 0,
+      },
+    };
+
+    return NextResponse.json(
+      {
+        data,
+        success: true,
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    NextResponse.json(
+      {
+        error,
+        message: "Internal Server Error",
+      },
+      { status: 500 },
+    );
+  }
+};
+
+export const POST = async (request: NextRequest) => {
+  const session = await getServerSideSession();
+  const url = new URL(request.url)
+  const postId = url.searchParams.get("postId") as string;
+  const body = (await request.json()) as { content: string };
+  const { content } = body;
+
+  try {
+    if(!postId) {
+      return NextResponse.json(
+        {
+          message: "Missing post id",
+          successs: false,
+        },
+        { status: 400 },
+      );
+    }
+
+    if (!content) {
+      return NextResponse.json(
+        {
+          message: "Missing comment content",
+          successs: false,
+        },
+        { status: 400 },
+      );
+    }
+
+    if (!session) {
+      return NextResponse.json(
+        {
+          message: "Unauthenticated, please log in first",
+          success: false,
+        },
+        { status: 400 },
+      );
+    }
+
+    const newComment = await db.comment.create({
+      data: {
+        postId,
+        userId: session.user.id,
+        content
+      }
+    })
+
+    return NextResponse.json(
+      {
+        data: newComment,
+        success: true,
+        message: "Comment created",
+      },
+      { status: 200 },
+    );
+
+  } catch (error) {
+    NextResponse.json(
+      {
+        error,
+        message: "Internal Server Error",
+        successs: false,
+      },
+      { status: 500 },
+    );
+  }
+};
