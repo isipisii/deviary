fc176a9539e4b94294dcc6e331993576bdecb005
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-08 21:08:41 +0800

    add PATCH and DELETE routes for comments

---
+++ src/app/api/comments/[commentId]/route.ts
@@ -0,0 +1,105 @@
+import { getServerSideSession } from "@/lib/auth";
+import { db } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+type TParams = {
+  params: {
+    commentId: string;
+  };
+};
+
+export const PATCH = async (request: NextRequest, { params }: TParams) => {
+  const session = await getServerSideSession();
+  const commentId = params.commentId;
+  const body = (await request.json()) as { content: string };
+  const { content } = body;
+
+  try {
+    if (!content) {
+      return NextResponse.json(
+        {
+          message: "Missing comment content",
+          successs: false,
+        },
+        { status: 400 },
+      );
+    }
+
+    if (!session) {
+      return NextResponse.json(
+        {
+          message: "Unauthenticated, please log in first",
+          success: false,
+        },
+        { status: 400 },
+      );
+    }
+
+    const updatedComment = await db.comment.update({
+      where: {
+        id: commentId,
+      },
+      data: {
+        content,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        data: updatedComment,
+        success: true,
+        message: "Comment updated",
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    NextResponse.json(
+      {
+        error,
+        message: "Internal Server Error",
+        successs: false,
+      },
+      { status: 500 },
+    );
+  }
+};
+
+export const DELETE = async (request: NextRequest, { params }: TParams) => {
+  const session = await getServerSideSession();
+  const commentId = params.commentId;
+
+  try {
+    if (!session) {
+      return NextResponse.json(
+        {
+          message: "Unauthenticated, please log in first",
+          success: false,
+        },
+        { status: 400 },
+      );
+    }
+
+    await db.comment.delete({
+      where: {
+        id: commentId,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Comment deleted",
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    NextResponse.json(
+      {
+        error,
+        message: "Internal Server Error",
+        successs: false,
+      },
+      { status: 500 },
+    );
+  }
+};
