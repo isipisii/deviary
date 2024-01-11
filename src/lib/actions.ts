8983aff842c5cd751a88ab7964dea65bcf2c9458
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-11 23:25:55 +0800

    create getPostById function

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,6 +1,27 @@
 "use server";
 import { revalidatePath } from "next/cache";
+import { db } from "./prisma";
 
 export const updateRoute = async (path: string) => {
   revalidatePath(path);
 };
+
+export async function getPostById(postId: string) {
+  return (await db.post.findUnique({
+    where: {
+      id: postId,
+    },
+    include: {
+      blog: true,
+      diary: true,
+      author: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          image: true,
+        },
+      },
+    },
+  })) as TPost;
+}
