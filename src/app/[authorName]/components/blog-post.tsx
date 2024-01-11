0555ddc6183296021f99ae5598e32cb9131583ba
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-11 23:26:45 +0800

    Refactor blog post component layout and styles

--- src/app/[authorName]/components/blog-post.tsx
+++ src/app/[authorName]/components/blog-post.tsx
@@ -14,28 +14,30 @@ export default function BlogPost({ post }: { post: TPost }) {
       <Button
         variant="light"
         size="md"
-        radius="lg"
         onClick={() => router.push("/feed")}
-        className="absolute left-5 top-5 font-semibold text-navTextColor"
-        startContent={<LuChevronLeft className="text-[1.5rem]" />}
+        className="absolute left-5 top-5 text-sm md:text-base font-semibold rounded-xl"
+        isIconOnly
       >
-        Go to feed
+        <LuChevronLeft className="md:text-[1.7rem] text-[1.5rem]" />
       </Button>
-
-      <div className="mt-10 flex gap-4 border p-4">
-        <PostActions post={post} orientation="vertical" />
+      
+      <div className="mt-20 md:mt-10 flex gap-4 w-full items-center justify-center">
+        {/* <PostActions post={post} orientation="vertical" /> */}
         <div className="flex w-full max-w-[900px] flex-col gap-4">
           <img
             src={post.blog?.thumbnail?.imageUrl as string}
             alt="thumbnail"
-            className="h-[180px] w-full rounded-tl-2xl rounded-tr-2xl object-cover md:h-[300px]"
+            className="h-[180px] w-full rounded-tl-2xl rounded-tr-2xl object-cover md:h-[300px] px-[20px]"
           />
 
           <div className="w-full">
-            <div className="flex flex-col gap-4">
-              <h1 className="text-[2rem] font-bold md:text-[2.5rem]">
-                {post.blog?.title}
-              </h1>
+            <div className="flex flex-col gap-4 px-[20px]">
+              <div>
+                <h1 className="text-[2rem] font-bold md:text-[2.5rem]">
+                  {post.blog?.title}
+                </h1>
+                <p className="text-navTextColor text-sm">{post.author.name}</p>
+              </div>
               <div className="flex gap-2">
                 {post.tags.map((tag, index) => (
                   <Chip
@@ -56,6 +58,11 @@ export default function BlogPost({ post }: { post: TPost }) {
             <MarkdownPreview modelValue={post.blog?.content as string} />
           </div>
         </div>
+        
+        {/* <div className="border h-[200px] w-[200px]">
+            
+        </div> */}
+
       </div>
     </>
   );
