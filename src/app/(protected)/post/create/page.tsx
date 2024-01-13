4408356088d5c72853ec5dd53853a53b624ba4ef
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-13 13:16:07 +0800

    add designs

--- src/app/(protected)/post/create/page.tsx
+++ src/app/(protected)/post/create/page.tsx
@@ -1,14 +1,15 @@
 import CreatePostContainer from "../components/create-post-container";
 import PageTitle from "@/components/ui/page-title";
 
-export default async function CreatePost() {
-  // TODO
+export default function CreatePost() {
   return (
-    <div className='p-6 md:p-12 w-full flex items-center justify-center'>
-      <div className="flex justify-between max-w-[900px] w-full flex-col gap-4">
-          <PageTitle>Create a post</PageTitle>
-          <CreatePostContainer />
-      </div> 
+    <div className="flex w-full items-center justify-center p-6 md:p-12">
+      <div className="z-[5] flex w-full max-w-[900px] flex-col justify-between gap-4">
+        <PageTitle>Create a post</PageTitle>
+        <CreatePostContainer />
+      </div>
+      <div className="fixed -bottom-[10rem] -right-[20rem] z-[0] h-[500px] w-[500px] rounded-full bg-gradient-to-l from-[#dd0dba31] to-[#dd0dba18] blur-[70px] filter lg:h-[800px] lg:w-[800px]" />
+      <div className="fixed -left-[10rem] -top-10 z-[0] h-[350px] w-[400px] rounded-full bg-gradient-to-l from-[#0d8add2b] to-[#0d8add1e] blur-[70px] filter lg:h-[700px] lg:w-[700px]" />
     </div>
-  )
+  );
 }
