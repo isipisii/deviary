2c41c9a9fe2d4519011d6c141d99cb6164690709
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-14 22:19:15 +0800

    add margin bottom

--- src/components/layout/card-container.tsx
+++ src/components/layout/card-container.tsx
@@ -6,7 +6,7 @@ export default function CardContainer({ children }: { children: ReactNode }) {
   return (
     // <div className="flex-start mt-8 flex flex-wrap items-center justify-center gap-6">
     <div
-      className={`mt-8 grid w-full grid-cols-1 place-items-center gap-8 ${
+      className={`mt-8 mb-[5rem] md:mb-0 grid w-full grid-cols-1 place-items-center gap-8 ${
         isSideBarMinimized
           ? "md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5"
           : "md:grid-cols-1 lg:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4"
