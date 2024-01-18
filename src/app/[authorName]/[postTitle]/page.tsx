599245661d464fb5a7155b807d2b385a8e27d286
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-18 21:12:08 +0800

    Add server-side session authentication to PostPage

--- src/app/[authorName]/[postTitle]/page.tsx
+++ src/app/[authorName]/[postTitle]/page.tsx
@@ -1,14 +1,16 @@
 import TopNav from "@/components/layout/top-nav";
 import BlogPost from "../components/blog-post";
 import { getPostById } from "@/lib/actions";
+import { getServerSideSession } from "@/lib/auth";
 
 export default async function PostPage({
   params,
 }: {
   params: { authorName: string; postTitle: string };
 }) {
+  const session = await getServerSideSession()
   const postId = params.postTitle.split("-").at(-1) as string;
-  const post = await getPostById(postId)
+  const post = await getPostById(postId, session?.user.id as string)
 
   return (
     <>
