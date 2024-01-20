8a78c26a8e0d58d5b1a07964a3d8c357e29a44a2
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-20 22:14:06 +0800

    Refactor post-actions component to handle upvoting

--- src/components/ui/post-actions.tsx
+++ src/components/ui/post-actions.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { Tooltip, Button } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 import { useRouter } from "next-nprogress-bar";
 import { FaRegComments } from "react-icons/fa";
-import { TbArrowBigUp, TbShare3 } from "react-icons/tb";
+import { TbArrowBigUp, TbArrowBigUpFilled, TbShare3 } from "react-icons/tb";
 import { cn } from "@/utils/cn";
 import { formatTitleWithId } from "@/utils/fornatTitleWithId";
 import CustomTooltip from "./custom-tooltip";
+import { useState } from "react";
+import { useUpvote, useRemoveUpvote } from "@/lib/services/upvote.api";
 
 type TOrientation = "vertical" | "horizontal";
 
@@ -22,34 +24,50 @@ export default function PostActions({
   post: TPost;
   orientation?: TOrientation;
 }) {
-  const selectedOrientation = orientationTypes[orientation ?? "horizontal"];
   const router = useRouter();
+  const selectedOrientation = orientationTypes[orientation ?? "horizontal"];
   const href = `/@${post.author.name.split(" ").join(".")}/${
     post.blog
       ? formatTitleWithId(post.blog?.title, post.id)
       : formatTitleWithId(post.diary?.title as string, post.id)
   }`;
 
+  const { mutate: upvoteMutation, isPending } = useUpvote();
+  const { mutate: removeUpvoteMutation } = useRemoveUpvote();
+  // const [upvoteCount, setUpvoteCount] = useState(post.upvoteCount ?? 0)
+  // const [isUpvoted, setIsUpvoted] = useState(post.isUpvoted);
+
+  function handleToggleUpvote() {
+    if (post.isUpvoted) {
+      removeUpvoteMutation(post.id)
+      return 
+    }
+    upvoteMutation(post.id);
+  }
 
   return (
     <div className={cn("flex items-center", selectedOrientation)}>
-      <CustomTooltip
-        placement="bottom"
-        content="Upvote"
-      >
-        <Button
-          isIconOnly
-          className="rounded-xl bg-[#fff0] text-2xl text-[#A1A1AA] 
-                hover:bg-[#34b60058] hover:text-[#34FF00]"
-        >
-          <TbArrowBigUp />
-        </Button>
-      </CustomTooltip>
+      <div className="flex items-center gap-1">
+        <CustomTooltip placement="bottom" content="Upvote">
+          <Button
+            isIconOnly
+            className={cn(
+              `rounded-xl bg-[#fff0] text-2xl text-[#A1A1AA]
+             hover:bg-[#34b6003b] hover:text-[#34FF00]`,
+              {
+                "text-[#34FF00]": post.isUpvoted,
+                "text-[#A1A1AA]": !post.isUpvoted,
+              },
+            )}
+            onClick={handleToggleUpvote}
+          >
+            {post.isUpvoted ? <TbArrowBigUpFilled /> : <TbArrowBigUp />}
+          </Button>
+        </CustomTooltip>
+        <p className="text-[#A1A1AA]">{post.upvoteCount}</p>
+      </div>
 
-      <CustomTooltip
-        placement="bottom"
-        content="Comments"
-      >
+      <CustomTooltip placement="bottom" content="Comments">
         <Button
           isIconOnly
           className="rounded-xl bg-[#fff0] text-2xl text-[#A1A1AA] 
@@ -60,11 +78,7 @@ export default function PostActions({
         </Button>
       </CustomTooltip>
 
-      
-      <CustomTooltip
-        placement="bottom"
-        content="Share"
-      >
+      <CustomTooltip placement="bottom" content="Share">
         <Button
           isIconOnly
           className="rounded-xl bg-[#fff0] text-2xl text-[#A1A1AA] 
