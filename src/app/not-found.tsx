18ca7fc7b7913f11d71e4edfce52692765045801
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-21 22:26:47 +0800

    add not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
 
-export default function page() {
+export default function NotFound() {
   return (
-    <div>not-found</div>
+    <div>
+      not found
+    </div>
   )
 }
