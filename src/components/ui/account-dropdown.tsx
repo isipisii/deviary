aab3baf78fef238103833e9eef2dfd9ce156e6fc
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-21 22:24:40 +0800

    Add logout confirmation modal

--- src/components/ui/account-dropdown.tsx
+++ src/components/ui/account-dropdown.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+import { useState } from "react";
 import {
   Dropdown,
   DropdownTrigger,
@@ -9,52 +10,83 @@ import {
   DropdownItem,
   User,
   DropdownSection,
+  useDisclosure,
 } from "@nextui-org/react";
 import { signOut } from "next-auth/react";
-import { useRouter } from 'next-nprogress-bar';
+import { useRouter } from "next-nprogress-bar";
 
 import { PiSignOutBold } from "react-icons/pi";
 import { LuSettings, LuUser2 } from "react-icons/lu";
+import ConfirmationModal from "./confirmation-modal";
 
 export default function AccountDropdown() {
   const { data } = useSession();
-  const router = useRouter()
+  const router = useRouter();
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+  const [isPending, setIsPending] = useState(false)
 
+  async function handleLogout(){
+    try {
+      setIsPending(true)
+      await signOut();
+      setIsPending(false)
+
+      router.push("/sign-in");
+    } catch (error) {
+      console.log(error)
+    }
+  
+  }
   return (
-    <Dropdown placement="bottom-start" backdrop="transparent" className="bg-background">
-      <DropdownTrigger>
-        <User
-          as="button"
-          avatarProps={{
-            src: data?.user?.image ?? "",
-          }}
-          className="transition-transform"
-          description={data?.user.email}
-          name={data?.user.name}
-        />
-      </DropdownTrigger>
-      <DropdownMenu aria-label="User Actions" variant="flat">
-        <DropdownSection showDivider>
-          <DropdownItem key="profile" startContent={<LuUser2 />}>
-            Profile
+    <>
+      <ConfirmationModal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        action={handleLogout}
+        isPending={isPending}
+        modalTextContent={{
+          header: "Log out?",
+          body: "Are you sure you want to log out?",
+        }}
+        isDelete={false}
+      />
+
+      <Dropdown
+        placement="bottom-start"
+        backdrop="transparent"
+        className="bg-background"
+      >
+        <DropdownTrigger>
+          <User
+            as="button"
+            avatarProps={{
+              src: data?.user?.image ?? "",
+            }}
+            className="transition-transform"
+            description={data?.user.email}
+            name={data?.user.name}
+          />
+        </DropdownTrigger>
+        <DropdownMenu aria-label="User Actions" variant="flat">
+          <DropdownSection showDivider>
+            <DropdownItem key="profile" startContent={<LuUser2 />}>
+              Profile
+            </DropdownItem>
+            <DropdownItem key="settings" startContent={<LuSettings />}>
+              My Settings
+            </DropdownItem>
+          </DropdownSection>
+          <DropdownItem
+            startContent={<PiSignOutBold />}
+            key="logout"
+            color="danger"
+            onClick={onOpen}
+            className="text-danger"
+          >
+            Log Out
           </DropdownItem>
-          <DropdownItem key="settings" startContent={<LuSettings />}>
-            My Settings
-          </DropdownItem>
-        </DropdownSection>
-        <DropdownItem 
-          startContent={<PiSignOutBold  />}
-          key="logout" 
-          color="danger" 
-          onClick={async () =>  {
-            await signOut()
-            router.push("/sign-in")
-          }}
-          className="text-danger"
-        >
-          Log Out
-        </DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
+        </DropdownMenu>
+      </Dropdown>
+    </>
   );
 }
