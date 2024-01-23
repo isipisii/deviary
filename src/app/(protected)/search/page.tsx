e1d06ea93cca46467c5e301726325fbb065cf67e
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-23 21:02:10 +0800

    add prefecth for posts in search page

--- src/app/(protected)/search/page.tsx
+++ src/app/(protected)/search/page.tsx
@@ -1,30 +1,29 @@
 import PageTitle from "@/components/ui/page-title";
-import { Input } from "@nextui-org/react";
-import { FaMagnifyingGlass } from "react-icons/fa6";
+import { QueryKeys } from "@/lib/constants";
+import { getFeedPosts } from "@/lib/services/post.api";
+import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
+import SearchContainer from "./components/search-container";
+import SearchField from "./components/search-field";
 
-export default function SearchPage() {
+export default async function SearchPage() {
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: [QueryKeys.Posts],
+    initialPageParam: "",
+    queryFn: ({ pageParam: lastCursor }) => getFeedPosts(5, lastCursor),
+    getNextPageParam: (lastPage: TPage<TPost[]>) => lastPage.metaData ? lastPage?.metaData.lastCursor : null,
+  })
+
   return (
     <div className="p-6 md:p-12">
       <div className="flex gap-4 flex-col">
         <PageTitle>Search</PageTitle>
-        <Input
-          labelPlacement="inside"
-          label="Find posts that interests you"
-          radius="lg"
-          variant="bordered"
-          size="sm"
-          className="max-w-md"
-          classNames={{
-            label: "font-semibold",
-            inputWrapper: "border-borderColor border-2 rounded-xl",
-          }}
-          // value={tag}
-          // onChange={(e) => setTag(e.target.value)}
-          endContent={
-            <FaMagnifyingGlass className="mb-[.6rem] text-navTextColor" />
-          }
-        />
+        <SearchField />
       </div>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <SearchContainer />
+      </HydrationBoundary>
     </div>
   );
 }
