b6beaa31164d6446da5c9290b0167f536c5f6997
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-24 17:12:00 +0800

    Refactor confirmation modal and page title components

--- src/components/ui/page-title.tsx
+++ src/components/ui/page-title.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react"
 
 export default function PageTitle({ children }: { children: ReactNode }) {
   return (
-    <h2 className="font-semibold text-3xl">
+    <h2 className="font-semibold text-2xl md:text-3xl">
         {children}
     </h2>
   )
