12059725d84abe79fd12837ce9fb92df5591ce05
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-24 21:22:18 +0800

    Refactor logo component to support system theme

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,15 +1,30 @@
 import { useTheme } from "next-themes";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Logo() {
-  const { theme } = useTheme()
+  const { theme } = useTheme();
+  const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [isSystemDark, setIsSystemDark] = useState(isDarkMode);
+
+  useEffect(() => {
+    setIsSystemDark(isDarkMode);
+  }, [theme, isDarkMode]);
 
   return (
     <Image
-        src={theme === "dark" || theme === undefined ? "/images/deviary-dark.png" : "/images/deviary-light.png"}
-        alt="logo"
-        width={90}
-        height={30}
+      src={
+        theme === "system"
+          ? isSystemDark
+            ? "/images/deviary-dark.png"
+            : "/images/deviary-light.png"
+          : theme === "dark" || theme === undefined
+            ? "/images/deviary-dark.png"
+            : "/images/deviary-light.png"
+      }
+      alt="logo"
+      width={90}
+      height={30}
     />
-  )
+  );
 }
