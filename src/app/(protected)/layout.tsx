357f4aa4af7858677b665626571746f566a6e7de
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-24 17:11:03 +0800

    Remove unused import and code

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,12 +1,10 @@
 import { SideBar } from "@/components/layout/sidebar-nav";
 import TopNav from "@/components/layout/top-nav";
-import { getServerSideSession } from "@/lib/auth";
 import LayoutWrapper from "@/components/layout/layout-wrapper";
 import { ReactNode } from "react";
 import MobileBottomNav from "@/components/layout/mobile-bottom-nav";
 
 export default async function Layout({ children }: { children: ReactNode }) {
-  const session = await getServerSideSession();
   return (
     <>
       <TopNav />
