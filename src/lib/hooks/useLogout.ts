74be96362e618f2de53946eba0e5f51f5bbbaf54
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-24 17:11:38 +0800

    Add useLogout hook for handling reusable logout functionality

---
+++ src/lib/hooks/useLogout.ts
@@ -0,0 +1,23 @@
+import { signOut } from "next-auth/react";
+import { useRouter } from "next-nprogress-bar";
+import { useState } from "react";
+
+
+export default function useLogout(onClose: () => void) {
+    const router = useRouter();
+    const [isPending, setIsPending] = useState(false);
+
+    async function handleLogout() {
+      try {
+        setIsPending(true);
+        await signOut();
+        setIsPending(false);
+        onClose();
+        router.push("/sign-in");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    return { isPending, handleLogout }
+}   
