a6340540e86855a5a64cabd6c0272ff37c28e4ab
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-26 20:26:42 +0800

    Commented out unused code in PostsSkeletonLoader component

--- src/components/shared/skeleton-loaders/posts-skeleton-loader.tsx
+++ src/components/shared/skeleton-loaders/posts-skeleton-loader.tsx
@@ -5,12 +5,12 @@ import PostSkeleton from "./post-skeleton";
 import { useState } from "react";
 
 export default function PostsSkeletonLoader({ isInInfinite = true }: { isInInfinite?: boolean }) {
-  const { isInRange } = useDetectViewport('sm')
-  const [arraySize] = useState(isInRange ? 4 : 8)
+  // const { isInRange } = useDetectViewport('sm')
+  // const [arraySize] = useState(isInRange ? 4 : 8)
 
   return (
     <>
-      {[...new Array(arraySize)].map((_, index) => {
+      {[...new Array(8)].map((_, index) => {
           return (
             <PostSkeleton key={"key" + index}/>
           );
