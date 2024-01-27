3fdb7c794a9fc4b0626cab968b400dbf7717ec07
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-27 23:08:23 +0800

    include some important fields

--- src/app/api/notifications/route.ts
+++ src/app/api/notifications/route.ts
@@ -2,6 +2,7 @@ import { db } from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 import { getServerSideSession } from "@/lib/auth";
 
+// TODO: create a ui for the notificatio tommorrow
 export const GET = async () => {
   const session = await getServerSideSession();
 
@@ -18,15 +19,24 @@ export const GET = async () => {
         recipientId: session.user.id,
       },
       include: {
-        sender: true,
+        sender: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            image: true,
+          },
+        },
+        post: {
+          include: {
+            blog: true,
+            diary: true,
+          },
+        },
       },
     });
 
-    return NextResponse.json(
-      { notifications, success: true },
-      { status: 200 },
-    );
-
+    return NextResponse.json({ notifications, success: true }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { message: "Internal Server Error", error },
