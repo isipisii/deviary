6007d759f1c6f2cf973814e1d95fd00f0c473ec5
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-28 22:20:40 +0800

    Add userSelectedFields and orderBy to GET notifications route

--- src/app/api/notifications/route.ts
+++ src/app/api/notifications/route.ts
@@ -2,6 +2,13 @@ import { db } from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 import { getServerSideSession } from "@/lib/auth";
 
+export const userSelectedFields = {
+  id: true,
+  name: true,
+  email: true,
+  image: true,
+}
+
 // TODO: create a ui for the notificatio tommorrow
 export const GET = async () => {
   const session = await getServerSideSession();
@@ -21,19 +28,24 @@ export const GET = async () => {
       include: {
         sender: {
           select: {
-            id: true,
-            name: true,
-            email: true,
-            image: true,
+           ...userSelectedFields
           },
         },
         post: {
           include: {
             blog: true,
             diary: true,
+            author: {
+              select: {
+                ...userSelectedFields
+              },
+            }
           },
         },
       },
+      orderBy: {
+        createdAt: "desc"
+      }
     });
 
     return NextResponse.json({ notifications, success: true }, { status: 200 });
