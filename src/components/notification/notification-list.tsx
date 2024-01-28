8dc11847afadefdbb039b2825633362fa1cb73d4
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-28 22:20:06 +0800

    Add NotificationList component

---
+++ src/components/notification/notification-list.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect } from "react";
+import { ScrollShadow } from "@nextui-org/react";
+import UpvoteNotificationCard from "./upvote-notification-card";
+
+export default function NotificationList({
+  notifications,
+  setAsViewed,
+}: {
+  notifications: TNotification[];
+  setAsViewed: () => void;
+}) {
+  useEffect(() => {
+    setAsViewed();
+  }, [setAsViewed]);
+
+  return (
+    <ScrollShadow size={90} className="h-[500px] w-full">
+      <div className="flex w-full flex-col gap-4">
+        {notifications && (
+          <div className="flex w-full flex-col gap-4">
+            {notifications.map((notification, index) => (
+              <UpvoteNotificationCard key={index} notification={notification} />
+            ))}
+          </div>
+        )}
+      </div>
+    </ScrollShadow>
+  );
+}
