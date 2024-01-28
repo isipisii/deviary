2a9694a3494af890732820d2fa396d26b53873f0
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-28 22:18:38 +0800

    Refactor UpvoteNotificationCard component

--- src/components/notification/upvote-notification-card.tsx
+++ src/components/notification/upvote-notification-card.tsx
@@ -1,44 +1,58 @@
-import formatDate from '@/utils/formatDate'
-import React from 'react'
-import { TbArrowBigUpFilled } from 'react-icons/tb'
+/* eslint-disable @next/next/no-img-element */
+import formatDate from "@/utils/formatDate";
+import React from "react";
+import { TbArrowBigUpFilled } from "react-icons/tb";
+import Link from "next/link";
+import { formatTitleWithId } from "@/utils/fornatTitleWithId";
 
-export default function UpvoteNotificationCard() {
+
+export default function UpvoteNotificationCard({
+  notification,
+}: {
+  notification: TNotification;
+}) {
+  const { sender, post, createdAt } = notification;
+  const href = `/@${post.author.name.split(" ").join(".")}/${
+    post.blog
+      ? formatTitleWithId(post.blog?.title, post.id)
+      : formatTitleWithId(post.diary?.title as string, post.id)
+  }`;
 
   return (
-    <div className="flex gap-3 py-3">
-    <div className="relative h-[45px] w-[45px] rounded-full">
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvP-MynRgvTDJw3MGfnb4E6BvL9QAWdRlP1ZIGdGeAlA&s"
-        alt="try"
-        className="w-full rounded-full"
-      />
-      <p className="absolute bottom-0 right-0 text-[1.1rem] text-[#34FF00]">
-        <TbArrowBigUpFilled />
-      </p>
-    </div>
+    <Link href={href}>
+      <div className="flex gap-3 py-3">
+        <div className="relative h-[45px] w-[45px] rounded-full">
+          <img src={sender.image} alt="try" className="w-full rounded-full" />
+          <p className="absolute bottom-0 right-0 text-[1.1rem] text-[#34FF00]">
+            <TbArrowBigUpFilled />
+          </p>
+        </div>
 
-    <div className="flex w-full flex-col gap-3">
-      <p className="text-sm">
-        <span className="font-semibold">Alessandro Benig</span>{" "}
-        <span className="text-navTextColor">
-          upvoted your post
-        </span>
-      </p>
-      <div className="flex items-center gap-2 rounded-lg border border-borderColor p-3">
-        <img
-          src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*th9ZIzkFpStwIUi4DIp8Ag.png"
-          alt="try"
-          className="h-[50px] w-[90px] rounded-md object-cover"
-        />
-        {/* post title */}
-        <p className="text-sm">
-          Recap of Next.js Conf 2023 and v14 release
-        </p>
+        <div className="flex w-full flex-col gap-3">
+          <p className="text-sm">
+            <span className="font-semibold">{sender.name}</span>{" "}
+            <span className="text-navTextColor">
+              upvoted your {post.type === "BLOG_POST" ? "blog" : "diary"} post
+            </span>
+          </p>
+          <div className="flex items-center gap-2 rounded-lg border border-borderColor p-3">
+            {post.type === "BLOG_POST" && (
+              <img
+                src={post.blog?.thumbnail?.imageUrl}
+                alt="try"
+                className="h-[50px] w-[90px] rounded-md object-cover"
+              />
+            )}
+            {/* post title */}
+            <p className="text-sm">
+              {post.type === "BLOG_POST" ? post.blog?.title : post.diary?.title}
+            </p>
+          </div>
+          <p className="self-end text-xs text-navTextColor">
+            {formatDate(createdAt)}
+          </p>
+        </div>
       </div>
-      <p className="self-end text-xs text-navTextColor">
-        {formatDate(new Date())}
-      </p>
-    </div>
-  </div>
-  )
+    </Link>
+  );
 }
