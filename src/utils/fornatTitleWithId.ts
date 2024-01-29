c3742c4a21dfcea87704ef966f16782b6d373e08
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-29 21:39:58 +0800

    Delete formatTitleWithId utility function

--- src/utils/fornatTitleWithId.ts
+++
@@ -1,6 +0,0 @@
-export function formatTitleWithId(title: string, postId: string) {
-    const titleArrayWithPostId = [...title.split(" "), postId];
-    const formattedTitleWithDash = titleArrayWithPostId.join("-");
-
-    return formattedTitleWithDash;
-}
