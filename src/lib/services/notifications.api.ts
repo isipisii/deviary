5ae86d8b13209dd38c9dfa24c79772bd694c10fc
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-29 21:43:58 +0800

    Add new notification-related  api hooks

--- src/lib/services/notifications.api.ts
+++ src/lib/services/notifications.api.ts
@@ -1,6 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { QueryClient, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios, { AxiosError }from "axios";
 import { QueryKeys } from "../constants";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 export function useGetNotifications() {
   return useQuery({
@@ -11,5 +13,50 @@ export function useGetNotifications() {
       return res.data.notifications as TNotification[];
     },
     staleTime: 60 * 1000,
+  });
+}
+
+export function useDeleteNotification() {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async (notificationId: string) => { 
+      return await axios.delete(`/api/notifications/${notificationId}`)
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: [QueryKeys.Notifications]})
+    },
+    onError: (error :AxiosError<ErrorResponse>) => {
+      toast.error("An error occured while deleting the notification")
+    }
+  })
+}
+
+export function useViewNotification() {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async (notificationId: string) => { 
+      return await axios.delete(`/api/notifications/view/${notificationId}`)
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: [QueryKeys.Notifications]})
+    },
+    onError: (error: AxiosError<ErrorResponse>) => {
+      toast.error("An error occured while deleting the notification")
+    }
+  })
+}
+
+export async function newNotificationOptimisticUpdate(queryClient: QueryClient, newNotification: TNotification) {
+  queryClient.setQueryData<TNotification[]>(
+    [QueryKeys.Notifications],
+    (oldData) => {
+      const newData = oldData ? [newNotification, ...oldData] : oldData;
+      return newData;
+    },
+  );
+  await queryClient.invalidateQueries({
+    queryKey: [QueryKeys.Notifications],
   });
 }
