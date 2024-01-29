9202c3a4b587b33e0837d72544e025a067894584
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-29 21:46:11 +0800

    Refactor UpvoteNotificationCard component

--- src/components/notification/upvote-notification-card.tsx
+++ src/components/notification/upvote-notification-card.tsx
@@ -3,8 +3,9 @@ import formatDate from "@/utils/formatDate";
 import React from "react";
 import { TbArrowBigUpFilled } from "react-icons/tb";
 import Link from "next/link";
-import { formatTitleWithId } from "@/utils/fornatTitleWithId";
-
+import NotificationContextMenu from "../ui/notification-context-menu";
+import formatPostHref from "@/utils/formatPostHref";
+import { Avatar } from "@nextui-org/react";
 
 export default function UpvoteNotificationCard({
   notification,
@@ -12,43 +13,50 @@ export default function UpvoteNotificationCard({
   notification: TNotification;
 }) {
   const { sender, post, createdAt } = notification;
-  const href = `/@${post.author.name.split(" ").join(".")}/${
-    post.blog
-      ? formatTitleWithId(post.blog?.title, post.id)
-      : formatTitleWithId(post.diary?.title as string, post.id)
-  }`;
 
   return (
-    <Link href={href}>
-      <div className="flex gap-3 py-3">
-        <div className="relative h-[45px] w-[45px] rounded-full">
-          <img src={sender.image} alt="try" className="w-full rounded-full" />
+    <Link href={formatPostHref(post)} className="w-full" 
+    >            
+      <div className="relative flex gap-3 py-3 w-full">
+        {!notification.viewed && (
+          <div className="absolute right-0 top-0 h-[10px] w-[10px] rounded-full bg-secondary" />
+        )}
+
+        <div className="relative md:h-[40px] md:w-[40px] h-[35px] w-[35px]">
+         <Avatar src={sender.image} className="md:h-[40px] md:w-[40px] h-[35px] w-[35px]" />
+          
           <p className="absolute bottom-0 right-0 text-[1.1rem] text-[#34FF00]">
             <TbArrowBigUpFilled />
           </p>
         </div>
 
-        <div className="flex w-full flex-col gap-3">
-          <p className="text-sm">
-            <span className="font-semibold">{sender.name}</span>{" "}
-            <span className="text-navTextColor">
-              upvoted your {post.type === "BLOG_POST" ? "blog" : "diary"} post
-            </span>
-          </p>
+        <div className="flex flex-col gap-3 w-[95%]">
+          <div className="flex items-center justify-between">
+            <p className="text-xs md:text-sm">
+              <Link href={`/profile/${sender.id}`} className="font-semibold">
+                {sender.name}
+              </Link>{" "}
+              <span className="text-navTextColor">
+                upvoted your {post.type === "BLOG_POST" ? "blog" : "diary"} post
+              </span>
+            </p>
+            <NotificationContextMenu notification={notification} />
+          </div>
+
           <div className="flex items-center gap-2 rounded-lg border border-borderColor p-3">
             {post.type === "BLOG_POST" && (
               <img
                 src={post.blog?.thumbnail?.imageUrl}
                 alt="try"
-                className="h-[50px] w-[90px] rounded-md object-cover"
+                className="h-[40px] w-[80px]  md:h-[50px] md:w-[90px] rounded-md object-cover"
               />
             )}
             {/* post title */}
-            <p className="text-sm">
+            <p className="text-xs md:text-sm">
               {post.type === "BLOG_POST" ? post.blog?.title : post.diary?.title}
             </p>
           </div>
-          <p className="self-end text-xs text-navTextColor">
+          <p className="self-end md:text-xs text-[.7rem] text-navTextColor">
             {formatDate(createdAt)}
           </p>
         </div>
