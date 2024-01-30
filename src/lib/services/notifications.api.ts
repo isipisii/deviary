195d4b8b759d44bae4a6071590cbee4dc8834630
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-30 16:17:57 +0800

    add optimistic update in notification when marking it as viewed

--- src/lib/services/notifications.api.ts
+++ src/lib/services/notifications.api.ts
@@ -1,5 +1,5 @@
 import { QueryClient, useQuery, useQueryClient } from "@tanstack/react-query";
-import axios, { AxiosError }from "axios";
+import axios, { AxiosError } from "axios";
 import { QueryKeys } from "../constants";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
@@ -17,38 +17,61 @@ export function useGetNotifications() {
 }
 
 export function useDeleteNotification() {
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: async (notificationId: string) => { 
-      return await axios.delete(`/api/notifications/${notificationId}`)
+    mutationFn: async (notificationId: string) => {
+      return await axios.delete(`/api/notifications/${notificationId}`);
     },
     onSuccess: async () => {
-      await queryClient.invalidateQueries({ queryKey: [QueryKeys.Notifications]})
+      await queryClient.invalidateQueries({
+        queryKey: [QueryKeys.Notifications],
+      });
     },
-    onError: (error :AxiosError<ErrorResponse>) => {
-      toast.error("An error occured while deleting the notification")
-    }
-  })
+    onError: (error: AxiosError<ErrorResponse>) => {
+      toast.error("An error occured while deleting the notification");
+    },
+  });
 }
 
 export function useViewNotification() {
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: async (notificationId: string) => { 
-      return await axios.delete(`/api/notifications/view/${notificationId}`)
+    mutationFn: async (notificationId: string) => {
+      return await axios.patch(`/api/notifications/view/${notificationId}`);
+    },
+    onMutate: async (notificationId) => {
+      await queryClient.cancelQueries({ queryKey: [QueryKeys.Notifications] });
+      const previousNotifications = queryClient.getQueryData([
+        QueryKeys.Notifications,
+      ]);
+      viewNotificationOptimisticUpdate(queryClient, notificationId);
+
+      return { previousNotifications };
     },
     onSuccess: async () => {
-      await queryClient.invalidateQueries({ queryKey: [QueryKeys.Notifications]})
+      await queryClient.invalidateQueries({
+        queryKey: [QueryKeys.Notifications],
+      });
     },
-    onError: (error: AxiosError<ErrorResponse>) => {
-      toast.error("An error occured while deleting the notification")
-    }
-  })
+    onError: (error, noitificationId, context) => {
+      if (context)
+        queryClient.setQueryData(
+          [QueryKeys.Notifications],
+          context?.previousNotifications,
+        );
+
+      console.log(error);
+      toast.error("An error occured while deleting the notification");
+    },
+  });
 }
 
-export async function newNotificationOptimisticUpdate(queryClient: QueryClient, newNotification: TNotification) {
+export async function newNotificationOptimisticUpdate(
+  queryClient: QueryClient,
+  newNotification: TNotification,
+) {
   queryClient.setQueryData<TNotification[]>(
     [QueryKeys.Notifications],
     (oldData) => {
@@ -59,4 +82,23 @@ export async function newNotificationOptimisticUpdate(queryClient: QueryClient, 
   await queryClient.invalidateQueries({
     queryKey: [QueryKeys.Notifications],
   });
+}
+
+function viewNotificationOptimisticUpdate(
+  queryClient: QueryClient,
+  notificationId: string,
+) {
+  queryClient.setQueryData<TNotification[]>(
+    [QueryKeys.Notifications],
+    (oldData) => {
+      const newData = oldData
+        ? oldData.map((notification) =>
+            notification.id === notificationId
+              ? { ...notification, viewed: true }
+              : notification,
+          )
+        : oldData;
+      return newData;
+    },
+  );
 }
