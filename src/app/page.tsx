13eee043e41fd655a36388ef795e1674c50bc1e0
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-01-31 21:44:27 +0800

    style cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,21 @@
-import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@nextui-org/react";
+import Link from "next/link";
+import { LuUsers2 } from "react-icons/lu";
 
 export default function Home() {
   return (
     <main className="relative">
-      <div className="absolute -right-[20rem] z-0 h-[500px] w-[500px] translate-y-[60%] overflow-hidden rounded-full bg-gradient-to-l from-[#dd0dba4d] to-[#ffffff18] blur-[70px] filter lg:h-[800px] lg:w-[800px]" />
-      <div className="absolute -bottom-10 -left-[20rem] z-[0] h-[350px] w-[400px] rounded-full bg-gradient-to-l from-[#0d8add25] to-[#0d8add29] blur-[80px] filter lg:h-[700px] lg:w-[700px]" />
-      
+      <div className="absolute right-0 top-0 z-0 h-[350px] w-[300px] translate-y-[190%] overflow-hidden rounded-bl-[8rem] rounded-tl-[8rem] bg-gradient-to-l from-[#dd0dba2f] to-[#dd0dc125] blur-[70px] filter md:h-[500px] md:w-[450px] md:translate-y-[120%]" />
+      <div className="absolute -bottom-10 -left-[18rem] z-0  h-[500px] w-[500px]  rounded-full bg-gradient-to-l from-[#0d8add25] to-[#0d8add29] blur-[80px] filter md:h-[600px] md:w-[600px]" />
+
       {/* Hero Section */}
-      <section className="relative flex h-[65vh] w-full items-center justify-center">
+      <section className="relative flex h-[70vh] w-full items-center justify-center">
         {/* pattern */}
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#dd0dba64_1px,transparent_1px),linear-gradient(to_bottom,#dd0dba64_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#dd0dba64,transparent_1px),linear-gradient(to_bottom,#dd0dba64,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_70%_60%_at_60%_0%,#000_80%,transparent_110%)]" />
         <nav
           className="fixed left-1/2 top-4 z-40 w-[90%] max-w-[1200px] -translate-x-1/2 
-          rounded-[2rem] border-2 border-[#42393962] p-4 backdrop-blur-sm backdrop-saturate-150 md:bg-background/60"
+          rounded-[2rem] border-2 border-[#42393962] px-6 py-3 backdrop-blur-sm backdrop-saturate-150 md:bg-background/60"
         >
           <header className="flex w-full items-center justify-between">
             <Image
@@ -24,14 +25,16 @@ export default function Home() {
               height={25}
             />
 
-            <Button
-              color="secondary"
-              className="font-medium"
-              size="sm"
-              radius="lg"
-            >
-              Sign in
-            </Button>
+            <Link href="/sign-in">
+              <Button
+                color="secondary"
+                className="font-medium"
+                size="sm"
+                radius="lg"
+              >
+                Sign in
+              </Button>
+            </Link>
           </header>
         </nav>
 
@@ -44,7 +47,7 @@ export default function Home() {
               write, read, keep, and explore.
             </span>
           </h1>
-          <p className="mt-4 text-center text-xs text-[#7b7a7a] md:text-sm lg:text-base">
+          <p className="mt-4 text-center text-sm text-[#ECEDEE]/80 md:text-[1rem]">
             Tech tales to tell,{" "}
             <span className="font-semibold text-white">code diaries</span> to
             swell.{" "}
@@ -56,12 +59,53 @@ export default function Home() {
 
       <section className="flex w-full justify-center">
         {/* 2nd Section Container */}
-        <div className="h-screen w-[90%] max-w-[1200px]">
+        <div className=" w-[90%] max-w-[1200px]">
           {/* Cards Container */}
-          <div className="flex w-full items-center gap-4">
-            <div className="h-[200px] w-full rounded-xl border-1 border-[#42393962] p-4 backdrop-blur-sm backdrop-saturate-150 md:bg-[#343434]/20"></div>
-            <div className="h-[200px] w-full rounded-xl border-1 border-[#42393962] p-4 backdrop-blur-sm backdrop-saturate-150 md:bg-[#343434]/20"></div>
-            <div className="h-[200px] w-full rounded-xl  border-1 border-[#42393962] p-4 backdrop-blur-sm backdrop-saturate-150 md:bg-[#343434]/20"></div>
+          <div className="flex w-full flex-wrap items-center justify-center gap-4">
+            <div className="relative md:h-[170px] h-[140px] w-full overflow-hidden rounded-xl border-1 border-[#42393962] bg-[#343434]/20 backdrop-blur-sm backdrop-saturate-150 md:w-[40%] lg:w-[30%]">
+              <p className="absolute -bottom-2 -right-[1rem] z-0 -rotate-45 text-[5rem] text-[#9c9999]">
+                <LuUsers2 />
+              </p>
+
+              <div className="absolute left-4 top-4 z-20 h-full w-[90%] space-y-1 ">
+                <p className="text-[1.1rem] font-bold text-[#ECEDEE]">Community</p>
+                <p className="text-sm leading-10  text-[#9c9999]">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum
+                  non tempora vero! Aliquid soluta. Eum non tempora vero!
+                  Aliquid soluta.
+                </p>
+              </div>
+            </div>
+
+            <div className="relative md:h-[170px] h-[140px] w-full overflow-hidden rounded-xl border-1 border-[#42393962] bg-[#343434]/20 backdrop-blur-sm backdrop-saturate-150 md:w-[40%] lg:w-[30%]">
+              <p className="absolute -bottom-2 -right-[1rem] z-0 -rotate-45 text-[5rem] text-[#9c9999]">
+                <LuUsers2 />
+              </p>
+
+              <div className="absolute left-4 top-4 z-20 h-full w-[90%] space-y-1 ">
+                <p className="text-[1.1rem] font-bold text-[#ECEDEE]">Community</p>
+                <p className="text-sm leading-10  text-[#9c9999]">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum
+                  non tempora vero! Aliquid soluta. Eum non tempora vero!
+                  Aliquid soluta.
+                </p>
+              </div>
+            </div>
+
+            <div className="relative md:h-[170px] h-[140px] w-full overflow-hidden rounded-xl border-1 border-[#42393962] bg-[#343434]/20 backdrop-blur-sm backdrop-saturate-150 md:w-[40%] lg:w-[30%]">
+              <p className="absolute -bottom-2 -right-[1rem] z-0 -rotate-45 text-[5rem] text-[#9c9999]">
+                <LuUsers2 />
+              </p>
+
+              <div className="absolute left-4 top-4 z-20 h-full w-[90%] space-y-1 ">
+                <p className="text-[1.1rem] font-bold text-[#ECEDEE]">Community</p>
+                <p className="text-sm leading-10  text-[#9c9999]">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum
+                  non tempora vero! Aliquid soluta. Eum non tempora vero!
+                  Aliquid soluta.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </section>
