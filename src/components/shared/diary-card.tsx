f3fb144299ebce156a2be86cec25a1d1bbeaf364
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-03 14:20:01 +0800

    Add link to post and format post href

--- src/components/shared/diary-card.tsx
+++ src/components/shared/diary-card.tsx
@@ -6,6 +6,8 @@ import PostActions from "./post-actions";
 import { Avatar } from "@nextui-org/react";
 import PostContextMenu from "../ui/post-context-menu";
 import formatDate from "@/utils/formatDate";
+import formatPostHref from "@/utils/formatPostHref";
+import Link from "next/link";
 
 interface IDiaryCard {
   post: TPost;
@@ -13,46 +15,50 @@ interface IDiaryCard {
 
 export default function DiaryCard({ post }: IDiaryCard) {
   return (
-    <div
-      className="flex h-[400px] w-full max-w-[350px]
+    <Link href={formatPostHref(post)} className=" w-full max-w-[350px]">
+      <div
+        className="flex h-[400px] w-full
       flex-col justify-between gap-2 rounded-3xl border-2 
       border-borderColor bg-cardBg p-4 shadow-xl"
-    >
-      <div className="grid gap-2">
-        <div className="flex justify-between">
-          <div className="flex items-center gap-2">
-            <Avatar
-              src={post.author?.image as string}
-              className="h-[40px] w-[40px]"
-            />
-            <div>
-              <p className="text-sm font-semibold">{post.author?.name}</p>
-              <p className="text-[.7rem] text-[#878080]">
-                {formatDate(post.createdAt)}
-              </p>
+      >
+        <div className="grid gap-2">
+          <div className="flex justify-between">
+            <div className="flex items-center gap-2">
+              <Avatar
+                src={post.author?.image as string}
+                className="h-[40px] w-[40px]"
+              />
+              <div>
+                <p className="text-sm font-semibold">{post.author?.name}</p>
+                <p className="text-[.7rem] text-[#878080]">
+                  {formatDate(post.createdAt)}
+                </p>
+              </div>
             </div>
+            <PostContextMenu post={post} />
           </div>
-          <PostContextMenu post={post} />
+          <h1 className="line-clamp-3 text-sm font-bold">
+            {post.diary?.title}
+          </h1>
         </div>
-        <h1 className="line-clamp-3 text-sm font-bold">{post.diary?.title}</h1>
-      </div>
 
-      <div className="grid gap-1">
-        <SyntaxHighlighter
-          language="jsx"
-          style={dracula}
-          showLineNumbers
-          customStyle={{
-            fontSize: ".6rem",
-            overflow: "auto",
-            borderRadius: ".75rem",
-            height: "200px",
-          }}
-        >
-          {post.diary?.codeSnippet as string}
-        </SyntaxHighlighter>
-        <PostActions post={post} />
+        <div className="grid gap-1">
+          <SyntaxHighlighter
+            language="jsx"
+            style={dracula}
+            showLineNumbers
+            customStyle={{
+              fontSize: ".6rem",
+              overflow: "auto",
+              borderRadius: ".75rem",
+              height: "200px",
+            }}
+          >
+            {post.diary?.codeSnippet as string}
+          </SyntaxHighlighter>
+          <PostActions post={post} />
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
