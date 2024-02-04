cf50d17722050effaa31b363b74b9153eff412d5
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-04 21:34:10 +0800

    Refactor sign-in and sign-up pages

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,6 @@
 import SignInForm from "../components/sign-in-form"
 
 export default async function SignIn() {
-
   return (
     <SignInForm />
   )
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,14 +1,6 @@
-import { getServerSideSession } from "@/lib/auth"
 import SignUpForm from "../components/sign-up-form"
-import { redirect } from "next/navigation"
 
-export default async function SignUp() {
-  // const session = await getServerSideSession()
-
-  // if(session){
-  //   redirect("/")
-  // }
-  
+export default async function SignUp() {  
   return (
     <SignUpForm />
   )
