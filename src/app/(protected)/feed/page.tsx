535d1e65cb6120b6ae9b12b65314e86ecf515425
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-04 21:35:01 +0800

    Add number of initial pages to prefetch

--- src/app/(protected)/feed/page.tsx
+++ src/app/(protected)/feed/page.tsx
@@ -14,6 +14,7 @@ export default async function Feed() {
     initialPageParam: "",
     queryFn: ({ pageParam: lastCursor }) => getFeedPosts(5, lastCursor),
     getNextPageParam: (lastPage: TPage<TPost[]>) => lastPage.metaData ? lastPage?.metaData.lastCursor : null,
+    pages: 3
   })
 
   return (
