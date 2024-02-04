0becc535413e6948c2e29b6e7e32fa5a26997aa9
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-04 21:36:08 +0800

    Add React Query prefetching for post comments

--- src/app/[authorName]/[postTitle]/page.tsx
+++ src/app/[authorName]/[postTitle]/page.tsx
@@ -2,22 +2,41 @@ import TopNav from "@/components/layout/top-nav";
 import BlogPost from "../components/blog-post";
 import { getPostById } from "@/lib/actions";
 import { getServerSideSession } from "@/lib/auth";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { QueryKeys } from "@/lib/constants";
+import { getPostComments } from "@/lib/services/comments.api";
 
 export default async function PostPage({
   params,
 }: {
   params: { authorName: string; postTitle: string };
 }) {
-  const session = await getServerSideSession()
+  const queryClient = new QueryClient();
+  const session = await getServerSideSession();
   const postId = params.postTitle.split("-").at(-1) as string;
-  const post = await getPostById(postId, session?.user.id as string)
+  const post = await getPostById(postId, session?.user.id as string);
+
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: [QueryKeys.Comments],
+    initialPageParam: "",
+    queryFn: ({ pageParam: lastCursor }) => getPostComments(5, lastCursor, postId),
+    getNextPageParam: (lastPage: TPage<TComment[]>) =>
+      lastPage.metaData ? lastPage?.metaData.lastCursor : null,
+    pages: 3,
+  });
 
   return (
     <>
       <TopNav />
-      <div className="relative mt-[73px] flex items-center justify-center">
-        {post?.type === "BLOG_POST" ? <BlogPost post={post} /> : null}
-      </div>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <div className="relative mt-[73px] flex items-center justify-center">
+          {post?.type === "BLOG_POST" ? <BlogPost post={post} /> : null}
+        </div>
+      </HydrationBoundary>
     </>
   );
 }
