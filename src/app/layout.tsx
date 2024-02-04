414172524ea676eb752d64e9b3bbad5d0e6e651b
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-04 21:29:23 +0800

    Add Loader component to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Noto_Sans, Inter, Roboto } from "next/font/google";
+import HolyLoader from "holy-loader";
 import "./styles/globals.css";
 import Providers from "@/components/providers/providers";
 import { Toaster } from "sonner";
@@ -10,13 +11,21 @@ export const metadata: Metadata = {
   description: `a developer's diary and community`,
 };
 
-export default async function RootLayout({ children, }: {
+export default async function RootLayout({ children }: {
   children: React.ReactNode;
 }) {
 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={"bg-background"}>
+        <HolyLoader
+          height={4}
+          color="#DD0DB9"
+          speed={200}
+          showSpinner={false}
+          easing="linear"
+          boxShadow="2px"
+        />
         <Providers>
           <Toaster richColors expand={true} position="bottom-right" className="bg-background" />
           {children}
