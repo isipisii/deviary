84c48ffeee33c14dcef5be58ab902511f48e0d73
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-06 23:16:50 +0800

    Refactor logo component to use resolvedTheme

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,39 +1,14 @@
-"use client"
-
 import { useTheme } from "next-themes";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
 export default function Logo() {
-  const { theme } = useTheme();
-  const isDarkMode =
-    typeof window === "undefined"
-      ? null
-      : window.matchMedia("(prefers-color-scheme: dark)").matches;
-  const [isSystemDark, setIsSystemDark] = useState(isDarkMode);
-
-  useEffect(() => {
-    setIsSystemDark(isDarkMode);
-  }, [theme, isDarkMode]);
-
+  const { resolvedTheme } = useTheme()
   return (
-    <>
-      {typeof window !== "undefined" && (
-        <Image
-          src={
-            theme === "system"
-              ? isSystemDark
-                ? "/images/deviary-dark.png"
-                : "/images/deviary-light.png"
-              : theme === "dark" || theme === undefined
-                ? "/images/deviary-dark.png"
-                : "/images/deviary-light.png"
-          }
-          alt="logo"
-          width={90}
-          height={30}
-        />
-      )}
-    </>
+    <Image
+      src={resolvedTheme === "dark" ?  "/images/deviary-dark.png": "/images/deviary-light.png"}
+      alt="light logo"
+      width={90}
+      height={30}
+    />
   );
 }
