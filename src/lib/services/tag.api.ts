6cf4ecd3848116f4325a39eae597ba7418c36dab
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-09 15:19:42 +0800

    Add useInfiniteQuery and getPostsByTag functions

--- src/lib/services/tag.api.ts
+++ src/lib/services/tag.api.ts
@@ -1,5 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { QueryKeys } from "../constants";
 
 export function useSearchTags(searchTerm: string) {
   return useQuery({
@@ -14,4 +15,34 @@ export function useSearchTags(searchTerm: string) {
     staleTime: 1000,
     enabled: false,
   });
+}
+
+export async function getPostsByTag(
+  lastCursor: string,
+  take: number,
+  tagName: string,
+) {
+  const res = await axios.get(`/api/tags/${tagName}`, {
+    params: {
+      lastCursor,
+      take,
+    },
+  });
+  return res.data.data as TPage<TPost[]>;
+}
+
+export function useGetPostsByTag(tagName: string) {
+  return useInfiniteQuery({
+    queryKey: [QueryKeys.PostsByTag, tagName],
+    initialPageParam: "",
+    queryFn: ({ pageParam: lastCursor }) =>
+      getPostsByTag(lastCursor, 5, tagName),
+    getNextPageParam: (lastPage) =>
+      lastPage.metaData ? lastPage?.metaData.lastCursor : null,
+  });
+}
+
+
+function upvotePostsByTagOptimisticUpdate() {
+  
 }
