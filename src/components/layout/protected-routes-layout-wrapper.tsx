64ba9e798779dbf816fe0b5ed09e69b41873fa87
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-09 15:20:00 +0800

    Add ProtectedRoutesLayoutWrapper component

---
+++ src/components/layout/protected-routes-layout-wrapper.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ReactNode } from "react";
+import { useSideBarNavStore } from "@/lib/store/useSideBarNavStore";
+
+export default function ProtectedRoutesLayoutWrapper({ children }: { children: ReactNode }) {
+  const { isSideBarMinimized } = useSideBarNavStore((state) => state);
+
+  return (
+    <main
+      className={`${
+        isSideBarMinimized ? "md:ml-[90px]" : "md:ml-[290px]"
+      } mt-[75px] transition-all duration-1000 ease-in-out`}
+    >
+      {children}
+    </main>
+  );
+}
