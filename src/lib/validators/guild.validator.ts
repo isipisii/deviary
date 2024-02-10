674b6f3fc67b569dac8081f2ee48827703029373
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-10 23:56:09 +0800

    Update guild description character limit

--- src/lib/validators/guild.validator.ts
+++ src/lib/validators/guild.validator.ts
@@ -4,5 +4,5 @@ export const guildSchema = z.object({
     name: z.string()
     .min(1, { message: "Guild name is required" })
     .max(50, { message: "Guild name should not exceed to 50 characters"}),
-    description: z.string().max(500, { message: "Guild name should not exceed to 500 characters"})
+    description: z.string().max(300, { message: "Guild name should not exceed to 300 characters"}),
 })
