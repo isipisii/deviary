54749dd7aa7dda66e2b67a08ceb1437ef832a141
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-10 23:56:25 +0800

    Update CustomRadio component styles

--- src/components/ui/custom-radio.tsx
+++ src/components/ui/custom-radio.tsx
@@ -8,7 +8,7 @@ export function CustomRadio({ children, ...otherProps }: RadioProps) {
       classNames={{
         base: cn(
           "m-0 items-center",
-          "max-w-[300px] cursor-pointer rounded-xl gap-4 p-2 border-2 border-transparent",
+          "max-w-[400px] cursor-pointer rounded-xl gap-4 px-4 py-2 border-2 border-transparent",
           "data-[selected=true]:border-secondary data-[selected=true]:bg-secondary/5",
         ),
       }}
