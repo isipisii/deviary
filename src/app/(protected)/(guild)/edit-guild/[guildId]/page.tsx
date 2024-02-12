84166dd1be0623c100ce8f2c4ec3c3c2f7788466
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-12 22:39:04 +0800

    Add async/await to fetch guild to edit

--- src/app/(protected)/(guild)/edit-guild/[guildId]/page.tsx
+++ src/app/(protected)/(guild)/edit-guild/[guildId]/page.tsx
@@ -1,14 +1,16 @@
 import PageTitle from "@/components/ui/page-title";
 import React from "react";
 import GuildForm from "../../components/guild-form";
+import { getGuildById } from "@/lib/actions";
 
-export default function EditGuild({ params }: { params: { guildId: string } }) {
-  // TODO: FETCH THE GUILD TO EDIT
+export default async function EditGuild({ params }: { params: { guildId: string } }) {
+  const guildToEdit = await getGuildById(params.guildId)
+
   return (
     <div className="flex w-full items-center justify-center p-6 md:p-12">
       <div className="flex w-full max-w-[900px] flex-col gap-8">
         <PageTitle>Edit Guild</PageTitle>
-        <GuildForm />
+        <GuildForm isEditing={true} guildToEdit={guildToEdit} />
       </div>
     </div>
   );
