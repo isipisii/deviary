6d42796517410909e2e50964c85c465fa5fc6dee
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-13 22:52:00 +0800

    Refactor Guild component and add dynamic data loading

--- src/app/(protected)/(guild)/components/guild.tsx
+++ src/app/(protected)/(guild)/components/guild.tsx
@@ -1,10 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
+import { useGetGuildById } from "@/lib/services/guild.api";
 import { Avatar, AvatarGroup, Button } from "@nextui-org/react";
 import GuildContextMenu from "./guild-context-menu";
+import { useParams } from "next/navigation";
+import { AiOutlineUserAdd } from "react-icons/ai";
 
-export default function Guild({ guild }: { guild: TGuild }) {
+export default function Guild() {
+  const { guildId } = useParams() as { guildId: string };
+  const { data: guild, isLoading, isError } = useGetGuildById(guildId);
+
+  if (!guild || isLoading) {
+    return <div>Loading</div>;
+  }
+
   return (
     <div className="relative h-screen w-full overflow-x-hidden">
       {/* radial gradients */}
@@ -24,7 +34,7 @@ export default function Guild({ guild }: { guild: TGuild }) {
       {/* guild details */}
       <div className="z-[5] grid h-auto w-full place-items-center gap-4 border-b border-borderColor bg-background/30 p-6 backdrop-blur-xl md:p-12 lg:place-items-start ">
         <img
-          src={guild.image.imageUrl}
+          src={guild?.image.imageUrl}
           alt="guild-avatar"
           className=" h-[120px] w-[120px] rounded-full  bg-foreground object-cover p-1 shadow-sm md:h-[150px] md:w-[150px]"
         />
@@ -33,44 +43,53 @@ export default function Guild({ guild }: { guild: TGuild }) {
           <div className="grid place-items-center gap-4 lg:place-items-start">
             <div className="guild-details flex flex-col gap-4">
               <div>
-                <h2 className="text-[2rem] font-bold">{guild.name}</h2>
-                <p>@{guild.name.toLowerCase().replaceAll(" ", "")}</p>
+                <h2 className="text-[2rem] font-bold">{guild?.name}</h2>
+                <p>@{guild?.name.toLowerCase().replaceAll(" ", "")}</p>
               </div>
-              <p className="text-navTextColor">{guild.description}</p>
+              <p className="text-navTextColor">{guild?.description}</p>
             </div>
 
-            {/* TODO: MAKE THIS DYNAMIC */}
             <AvatarGroup
               max={5}
-              total={10}
+              total={guild.membersCount - 5}
               size="sm"
-              className="w-[210px]"
               renderCount={(count) => (
                 <p className="ms-2 text-sm font-medium text-foreground">
                   +{count} others
                 </p>
               )}
             >
-              <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
-              <Avatar src="https://i.pravatar.cc/150?u=a04258a2462d826712d" />
-              <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
-              <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026302d" />
-              <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
-              <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026708c" />
+              {guild?.members.map((member) => (
+                <Avatar src={member.user.image} key={member.id} />
+              ))}
             </AvatarGroup>
           </div>
-          
-          <div className="flex gap-2 items-center">
-            <Button 
+
+          <div className="flex items-center gap-2">
+            <Button
               size="md"
               color="secondary"
-              className="w-full max-w-[250px] rounded-xl font-medium text-white"
+              variant={guild.isBelong ? "bordered" : "solid"}
+              className={`w-full rounded-xl font-medium ${
+                guild.isBelong ? "text-secondary" : "text-white"
+              }`}
             >
-              Request to join
+              {guild.isBelong
+                ? "Joined"
+                : guild.isPrivate
+                  ? "Request to join"
+                  : "Join"}
             </Button>
+
+            <Button
+              size="md"
+              color="secondary"
+              className="w-full rounded-xl font-medium text-white"
+              isIconOnly
+              startContent={<AiOutlineUserAdd className="text-[1.2rem]" />}
+            />
             <GuildContextMenu />
           </div>
-         
         </div>
       </div>
     </div>
