c3dc86c7fd6f59f968b12f75edc8189d8d100dec
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-13 22:51:43 +0800

    Update z-index in NewGuild page

--- src/app/(protected)/(guild)/new-guild/page.tsx
+++ src/app/(protected)/(guild)/new-guild/page.tsx
@@ -4,7 +4,7 @@ import GuildForm from "../components/guild-form";
 export default function NewGuild() {
   return (
     <div className="flex w-full items-center justify-center p-6 md:p-12">
-      <div className="flex w-full max-w-[900px] flex-col gap-8">
+      <div className="flex w-full max-w-[900px] flex-col gap-8 z-[5]">
         <PageTitle>Create a new Guild</PageTitle>
         <GuildForm />
       </div>
