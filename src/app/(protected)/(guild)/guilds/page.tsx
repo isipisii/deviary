914d1c220ca7b8600e1d4b308b85230afe9728b6
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-14 22:21:05 +0800

    Add page title and prefetch guilds data

--- src/app/(protected)/(guild)/guilds/page.tsx
+++ src/app/(protected)/(guild)/guilds/page.tsx
@@ -1,6 +1,31 @@
+import PageTitle from "@/components/ui/page-title";
+import GuildsContainer from "../components/guilds-container";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { getGuilds } from "@/lib/services/guild.api";
+import { QueryKeys } from "@/lib/constants";
 
-export default function GuildsPage() {
+export default async function GuildsPage() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: [QueryKeys.Guilds],
+    initialPageParam: "",
+    queryFn: ({ pageParam: lastCursor }) => getGuilds(lastCursor, 5),
+    getNextPageParam: (lastPage) =>
+      lastPage.metaData ? lastPage?.metaData.lastCursor : null,
+    pages: 3,
+  });
+
   return (
-    <div>Guild</div>
-  )
+    <div className="p-6 md:p-12">
+      <PageTitle>Guilds</PageTitle>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <GuildsContainer />
+      </HydrationBoundary>
+    </div>
+  );
 }
