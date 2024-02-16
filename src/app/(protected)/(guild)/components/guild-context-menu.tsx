dcd2301cd8813064971af6aafe3af99904f706f0
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-16 14:59:35 +0800

    Refactor guild context menu to dynamically filter dropdown items based on guild membership

--- src/app/(protected)/(guild)/components/guild-context-menu.tsx
+++ src/app/(protected)/(guild)/components/guild-context-menu.tsx
@@ -10,7 +10,14 @@ import { GoKebabHorizontal } from "react-icons/go";
 import { PiSignOutBold } from "react-icons/pi";
 import { TbShare3 } from "react-icons/tb";
 
-export default function GuildContextMenu() {
+export default function GuildContextMenu({ guild }: { guild: TGuild }) {
+  const dropdownItems = [
+    { text: "Share guild", key: "share", icon: TbShare3 },
+    { text: "Leave guild", key: "leave", icon: PiSignOutBold },
+  ];
+  const filteredItems = guild.isBelong
+    ? dropdownItems
+    : dropdownItems.filter((item) => item.key !== "leave");
 
   return (
     <Dropdown
@@ -22,28 +29,34 @@ export default function GuildContextMenu() {
           variant="bordered"
           size="md"
           isIconOnly
-          className="z-[5] rounded-xl text-[1.2rem] border-1 border-borderColor"
+          className="z-[5] rounded-xl border-1 border-borderColor text-[1.2rem]"
           startContent={<GoKebabHorizontal />}
         />
       </DropdownTrigger>
-      <DropdownMenu variant="flat">
-        <DropdownItem
-          key="share"
-          className="rounded-lg"
-          startContent={<TbShare3 />}
-        >
-          Share guild
-        </DropdownItem>
-        <DropdownItem
-          key="leave"
-          className="rounded-lg text-danger"
-          color="danger"
-          startContent={
-            <PiSignOutBold />
+      <DropdownMenu variant="flat" items={filteredItems}>
+        {(item) => {
+          if (item.key === "share") {
+            return (
+              <DropdownItem
+                key={item.key}
+                className="rounded-lg"
+                startContent={<item.icon />}
+              >
+                {item.text}
+              </DropdownItem>
+            );
           }
-        >
-          Leave guild
-        </DropdownItem>
+          return (
+            <DropdownItem
+              key={item.key}
+              className="rounded-lg text-danger"
+              color="danger"
+              startContent={<item.icon />}
+            >
+              {item.text}
+            </DropdownItem>
+          );
+        }}
       </DropdownMenu>
     </Dropdown>
   );
