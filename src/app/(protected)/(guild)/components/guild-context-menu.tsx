0353fb92b52669630e861331eb969a2867a576bd
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-16 23:59:55 +0800

    Add leave guild functionality to GuildContextMenu

--- src/app/(protected)/(guild)/components/guild-context-menu.tsx
+++ src/app/(protected)/(guild)/components/guild-context-menu.tsx
@@ -9,8 +9,14 @@ import {
 import { GoKebabHorizontal } from "react-icons/go";
 import { PiSignOutBold } from "react-icons/pi";
 import { TbShare3 } from "react-icons/tb";
+import { useDisclosure } from "@nextui-org/react";
+import ConfirmationModal from "@/components/ui/confirmation-modal";
+import { useLeaveGuild } from "@/lib/services/guild.api";
 
 export default function GuildContextMenu({ guild }: { guild: TGuild }) {
+  const { isOpen, onOpenChange, onOpen, onClose } = useDisclosure();
+  const { mutate: leaveGuildMutation, isPending } = useLeaveGuild(onClose);
+  
   const dropdownItems = [
     { text: "Share guild", key: "share", icon: TbShare3 },
     { text: "Leave guild", key: "leave", icon: PiSignOutBold },
@@ -20,44 +26,60 @@ export default function GuildContextMenu({ guild }: { guild: TGuild }) {
     : dropdownItems.filter((item) => item.key !== "leave");
 
   return (
-    <Dropdown
-      className="rounded-xl border border-borderColor bg-background"
-      placement="bottom-end"
-    >
-      <DropdownTrigger>
-        <Button
-          variant="bordered"
-          size="md"
-          isIconOnly
-          className="z-[5] rounded-xl border-1 border-borderColor text-[1.2rem]"
-          startContent={<GoKebabHorizontal />}
-        />
-      </DropdownTrigger>
-      <DropdownMenu variant="flat" items={filteredItems}>
-        {(item) => {
-          if (item.key === "share") {
+    <>
+      <ConfirmationModal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        action={() => leaveGuildMutation(guild.id)}
+        isPending={isPending}
+        modalTextContent={{
+          header: "Leave guild?",
+          body: "Are you sure you want to leave this guild?",
+        }}
+        isDelete={false}
+        buttonText="Leave"
+      />
+
+      <Dropdown
+        className="rounded-xl border border-borderColor bg-background"
+        placement="bottom-end"
+      >
+        <DropdownTrigger>
+          <Button
+            variant="bordered"
+            size="md"
+            isIconOnly
+            className="z-[5] rounded-xl border-1 border-borderColor text-[1.2rem]"
+            startContent={<GoKebabHorizontal />}
+          />
+        </DropdownTrigger>
+        <DropdownMenu variant="flat" items={filteredItems}>
+          {(item) => {
+            if (item.key === "share") {
+              return (
+                <DropdownItem
+                  key={item.key}
+                  className="rounded-lg"
+                  startContent={<item.icon />}
+                >
+                  {item.text}
+                </DropdownItem>
+              );
+            }
             return (
               <DropdownItem
                 key={item.key}
-                className="rounded-lg"
+                className="rounded-lg text-danger"
+                color="danger"
                 startContent={<item.icon />}
+                onClick={onOpen}
               >
                 {item.text}
               </DropdownItem>
             );
-          }
-          return (
-            <DropdownItem
-              key={item.key}
-              className="rounded-lg text-danger"
-              color="danger"
-              startContent={<item.icon />}
-            >
-              {item.text}
-            </DropdownItem>
-          );
-        }}
-      </DropdownMenu>
-    </Dropdown>
+          }}
+        </DropdownMenu>
+      </Dropdown>
+    </>
   );
 }
