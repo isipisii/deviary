d6430eef8983fb4cc832d2323c3d54335c899916
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-17 00:00:13 +0800

    Remove unnecessary code in Guild component

--- src/app/(protected)/(guild)/components/guild.tsx
+++ src/app/(protected)/(guild)/components/guild.tsx
@@ -26,7 +26,6 @@ export default function Guild() {
         }}
       />
       {isLoading && <GuildDetailsSkeleton />}
-
       {/* guild details */}
       {guild && (
         <GuildDetails guild={guild} />
