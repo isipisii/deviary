5abcd643330de0a079b0936921c76475148f0593
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-17 22:42:20 +0800

    Add functions to check guild actions eligibility

---
+++ src/utils/checkGuildActionsEligibility.ts
@@ -0,0 +1,39 @@
+import { db } from "@/lib/prisma";
+
+export async function checkIfGuildCreator(userId: string, guildId: string) {
+    const existingMember = await db.guildMember.findFirst({
+        where: {
+            userId,
+            guildId
+        }
+    })
+    
+    if(!existingMember) {
+        throw new Error("Member not found");
+    }
+
+    if(existingMember.role === "CREATOR") {
+        return true
+    }
+
+    return false
+}
+
+export async function checkIfGuildModerator(userId: string, guildId: string) {
+    const existingMember = await db.guildMember.findFirst({
+        where: {
+            userId,
+            guildId
+        }
+    })
+    
+    if(!existingMember) {
+        throw new Error("Member not found");
+    }
+
+    if(existingMember.role === "MODERATOR") {
+        return true
+    }
+
+    return false
+}
