e47fc355d56868fac0bb0944e0d52f380d011292
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-17 22:45:47 +0800

    Add PopularPostsContainer component for displaying popular posts

---
+++ src/app/(protected)/popular/components/popular-posts-container.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import BlogPostCard from "@/components/shared/blog-post-card";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import { useGetPopularPosts } from "@/lib/services/post.api";
+import DiaryCard from "@/components/shared/diary-card";
+import PostsSkeletonLoader from "@/components/skeleton-loaders/posts-skeleton-loader";
+import CardContainer from "@/components/layout/card-container";
+
+export default function PopularPostsContainer() {
+  const { ref, inView } = useInView();
+  const { fetchNextPage, hasNextPage, isFetchingNextPage, data, isLoading } = useGetPopularPosts();
+
+  useEffect(() => {
+    // checks if the last element that has the ref and if theres next page in order to
+    // fetch the next page
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [hasNextPage, inView, fetchNextPage]);
+
+  return (
+    <div className="flex w-full flex-col items-center justify-center">
+      <CardContainer>
+        {data?.pages.map(
+          (page) =>
+            page.data?.map((post, index) => {
+              // this is for the last element in order to put the ref for infinite scrolling
+              if (page.data.length === index + 1) {
+                if (post.type === "BLOG_POST") {
+                  return (
+                    <div
+                      ref={ref}
+                      key={post.id}
+                      className="w-full max-w-[350px]"
+                    >
+                      <BlogPostCard post={post} />
+                    </div>
+                  );
+                }
+                return (
+                  <div ref={ref} key={post.id} className="w-full max-w-[350px]">
+                    <DiaryCard post={post} />
+                  </div>
+                );
+              }
+
+              if (post.type === "BLOG_POST") {
+                return <BlogPostCard post={post} key={post.id} />;
+              }
+              return <DiaryCard post={post} key={post.id} />;
+            }),
+        )}
+        {isLoading && <PostsSkeletonLoader />}
+        {isFetchingNextPage && <PostsSkeletonLoader isInInfinite />}
+      </CardContainer>
+    </div>
+  );
+}
