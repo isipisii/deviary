9357cc733ba84da2daf7736720124577c9b6b543
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-20 22:02:59 +0800

    Add user API services

---
+++ src/lib/services/user.api.ts
@@ -0,0 +1,36 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { QueryKeys } from "../constants";
+import axios, { AxiosError } from "axios";
+import { toast } from "sonner";
+
+export function useApplyFeedFilter() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (filter: string) => {
+      return await axios.patch(
+        "/api/users/filter-feed-posts",
+        {},
+        { params: { filter } },
+      );
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: [QueryKeys.Posts] });
+      await queryClient.invalidateQueries({
+        queryKey: [QueryKeys.CurrentFeedFilter],
+      });
+    },
+    onError: (error: AxiosError<ErrorResponse>) => {
+      toast.error(error.response?.data.message);
+    },
+  });
+}
+
+export function useGetCurrentFeedFilter() {
+  return useQuery({
+    queryKey: [QueryKeys.CurrentFeedFilter],
+    queryFn: async () => {
+      const res = await axios.get("/api/users/filter-feed-posts");
+      return res.data.currentFeedFilter as string;
+    },
+  });
+}
