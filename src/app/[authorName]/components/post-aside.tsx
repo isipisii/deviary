e6eb032340194f313b31c2db8f187bf9fcdc9cf0
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-24 10:25:05 +0800

    Add medal icon to author in post-aside component

--- src/app/[authorName]/components/post-aside.tsx
+++ src/app/[authorName]/components/post-aside.tsx
@@ -7,6 +7,7 @@ import { User} from "@nextui-org/react";
 import CommentList from "./comment/comment-list";
 import { useSession } from "next-auth/react";
 import CommentForm from "./comment/comment-form";
+import { FaMedal } from "react-icons/fa";
 
 export default function PostAside({ post }: { post: TPost }) {
   const [isCommentFormOpen, setIsCommentFormOpen] = useState(false);
@@ -21,28 +22,33 @@ export default function PostAside({ post }: { post: TPost }) {
       <div className="order-last flex flex-col gap-4 md:sticky md:top-[5.5rem] md:m-0">
         <div
           className=" flex 
-          h-[150px] w-full flex-col justify-between
-          rounded-3xl border border-borderColor p-4"
+          w-full flex-col justify-between
+          rounded-3xl border border-borderColor p-4 gap-4"
         >
           <div className="flex justify-between">
-            <User
-              as="button"
-              avatarProps={{
-                src: post.author.image ?? "",
-              }}
-              className="self-start transition-transform"
-              classNames={{
-                description: "text-navTextColor",
-                name: "font-medium",
-              }}
-              description={post.author.email}
-              name={post.author.name}
-            />
+            <div className="flex gap-2 items-center">
+              <User
+                as="button"
+                avatarProps={{
+                  src: post.author.image,
+                  isBordered: true
+                }}
+                className="self-start transition-transform"
+                classNames={{
+                  description: "text-navTextColor",
+                  name: "font-semibold",
+                }}
+                description={post.author.email}
+                name={post.author.name}
+              />
+              <p className="text-md text-warning -mt-1"><FaMedal/></p>
+            </div>
             <PostContextMenu
               post={post}
               className="border-[1px] border-borderColor bg-background"
             />
           </div>
+          <p className="text-navTextColor">{post.author.bio}</p>
           <PostActions
             post={post}
             isInPostPage={true}
