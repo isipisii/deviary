46f0d2aae2571eb4c73af6ce5c9da423253600a2
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-25 23:16:19 +0800

    Refactor CommentReplies component to include postAuthorId in CommentCard props

--- src/app/[authorName]/components/comment/comment-replies.tsx
+++ src/app/[authorName]/components/comment/comment-replies.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import CommentCard from "./comment-card";
 
-export default function CommentReplies({ commentReplies }: { commentReplies: TComment[] }) {
+export default function CommentReplies({ commentReplies, postAuthorId }: { commentReplies: TComment[], postAuthorId: string }) {
   return (
-    <div className="flex gap-4 flex-col">
-        {commentReplies.map((commentReply, index)=> (
-          <CommentCard key={index} comment={commentReply} />
-        ))}
+    <div className="flex gap-4 flex-col pl-4">
+      {commentReplies.map((commentReply, index)=> (
+        <CommentCard key={index} comment={commentReply} postAuthorId={postAuthorId} isReply={true} />
+      ))}
     </div>
   )
 }
