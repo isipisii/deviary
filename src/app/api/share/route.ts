b90fe8509a75442177c0a2bfd94115602b0cacd1
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-26 23:11:37 +0800

    Add PATCH method to update shared post content

--- src/app/api/share/route.ts
+++ src/app/api/share/route.ts
@@ -1,12 +1,14 @@
 import { db } from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 import { getServerSideSession } from "@/lib/auth";
+import { checkIfGuildMember } from "@/utils/checkGuildActionsEligibility";
 
 export const POST = async (req: NextRequest) => {
     const session = await getServerSideSession()
     const url = new URL(req.url)
     const guildId = url.searchParams.get("guildId")
     const postId = url.searchParams.get("postId")
+    const { content } = await req.json() as { content: string }
 
     try {
         if(!session) {
@@ -22,7 +24,6 @@ export const POST = async (req: NextRequest) => {
                 success: false
             }, { status: 400 })
         }
-
         
         if(!postId) {
             return NextResponse.json({
@@ -31,16 +32,70 @@ export const POST = async (req: NextRequest) => {
             }, { status: 400 })
         }
 
+        const userId = session.user.id
+        const isGuildMember = await checkIfGuildMember(userId, guildId)
+
+        if(!isGuildMember) {
+            return NextResponse.json({
+                message: "Unauthorized, you're not a member of the guild",
+                success: false
+            }, { status: 403 })
+        }
+
         await db.guildShare.create({
             data: {
+                content,
                 postId,
                 guildId,
-                userId: session.user.id
+                userId: userId
             }
         })
         
         return NextResponse.json({
             message: "Post has been shared",
+            success: true
+        }, { status: 200 })
+
+    } catch (error) {
+        return NextResponse.json({
+            message: "Internal Server Error",
+            success: false
+        }, { status: 500 })
+    }
+}
+
+export const PATCH = async (req: NextRequest) => {
+    const session = await getServerSideSession()
+    const url = new URL(req.url)
+    const shareId = url.searchParams.get("shareId")
+    const { content } = await req.json() as { content: string }
+
+    try {
+        if(!session) {
+            return NextResponse.json({
+                message: "Unauthenticated",
+                success: false
+            }, { status: 401 })
+        }
+
+        if(!shareId) {
+            return NextResponse.json({
+                message: "Missing guild id",
+                success: false
+            }, { status: 400 })
+        }
+    
+        await db.guildShare.update({
+            where: {
+                id: shareId
+            },
+            data: {
+                content
+            }
+        })
+        
+        return NextResponse.json({
+            message: "Shared post edited",
             success: true
         }, { status: 200 })
 
