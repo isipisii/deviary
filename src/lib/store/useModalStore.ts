0356415f235e9febabe2c6839207e1e6f69e50f4
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-26 23:10:35 +0800

    Add useModalStore to manage modal state

---
+++ src/lib/store/useModalStore.ts
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+
+type TSideBarNavState = {
+    isShareModalOpen: boolean
+    openShareModal: () => void
+    closeShareModal: () => void
+    onOpenChangeShareModal: () => void
+    setPostToShare: (post: TPost) => void
+    postToShare: TPost | null
+}
+
+export const useModalStore = create<TSideBarNavState>((set, get) => ({
+    isShareModalOpen: false,
+    postToShare: null,
+
+    openShareModal: () => set({isShareModalOpen: true}),
+    closeShareModal: () => set({isShareModalOpen: false}),
+    onOpenChangeShareModal: () => set({ isShareModalOpen: !get().isShareModalOpen }),
+    setPostToShare: (post: TPost) => set({ postToShare: post })
+}))
