60f2cbdf0c984d326b030efe2482f4fe8f8f9c40
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-28 22:12:06 +0800

    Add selectedGuild state and close modal functionality

--- src/components/ui/share-modal.tsx
+++ src/components/ui/share-modal.tsx
@@ -16,15 +16,29 @@ import { toast } from "sonner";
 import { LuCopy } from "react-icons/lu";
 import { FaCheck } from "react-icons/fa6";
 import { useState } from "react";
+import { FaArrowLeft } from "react-icons/fa6";
+import SharePostForm from "@/app/(protected)/(guild)/components/share-post-form";
 
 export default function ShareModal() {
   const queryClient = useQueryClient();
   const [isCopied, setIsCopied] = useState(false);
+  const [selectedGuild, setSelectedGuild] = useState<TGuild | null>(null);
   const userGuilds = queryClient.getQueryData<{ guild: TGuild }[]>([
     QueryKeys.MyGuilds,
   ]);
-  const { isShareModalOpen, onOpenChangeShareModal, postToShare } =
-    useModalStore((state) => state);
+  const {
+    isShareModalOpen,
+    onOpenChangeShareModal,
+    postToShare,
+    setPostToShare,
+    closeShareModal,
+  } = useModalStore((state) => state);
+
+  function closeModal() {
+    setPostToShare(null);
+    closeShareModal();
+    setSelectedGuild(null);
+  }
 
   async function handleCopyToClipboard() {
     if (!postToShare) return;
@@ -50,78 +64,91 @@ export default function ShareModal() {
         backdrop: "bg-[#292f46]/50 backdrop-opacity-30",
         base: "border-[#292f46] bg-cardBg rounded-2xl mx-4 py-3",
       }}
-      size="xl"
+      size={selectedGuild ? "sm" : "xl"}
     >
       <ModalContent>
         {(onClose) => (
           <>
-            <ModalHeader className="flex flex-col gap-1">
-              <h2 className="text-xl">Share Post</h2>
+            <ModalHeader className="flex items-center gap-[5rem]">
+              {selectedGuild && (
+                <Button
+                  variant="light"
+                  size="sm"
+                  onClick={() => setSelectedGuild(null)}
+                  radius="lg"
+                  isIconOnly
+                  className="text-lg"
+                >
+                  <FaArrowLeft />
+                </Button>
+              )}
+              <h2 className="text-xl ">Share Post</h2>
             </ModalHeader>
             <ModalBody>
-              <div className="flex flex-col gap-4">
-                <div className="flex flex-col gap-2">
-                  <h3 className="text-sm font-semibold text-navTextColor">
-                    Share to guild
-                  </h3>
+              {/* <div className="flex overflow-hidden"> */}
+              {/* right */}
+              {!selectedGuild && (
+                <div className="flex w-full flex-col gap-4">
+                  <div className="flex flex-col gap-2">
+                    <h3 className="text-sm font-semibold text-navTextColor">
+                      Share to guild
+                    </h3>
 
-                  <div className="flex gap-3">
-                    {userGuilds?.map((guild, index) => (
-                      <div
-                        key={index}
-                        className="flex flex-col flex-wrap items-center gap-1"
-                      >
-                        <div className="rounded-md p-2">
-                          <Avatar
-                            isBordered
-                            src={guild.guild.image.imageUrl}
-                            alt="guild-image"
-                            className="h-[40px] w-[40px]"
-                          />
+                    <div className="flex gap-3">
+                      {userGuilds?.map((guild, index) => (
+                        <div
+                          key={index}
+                          className="flex flex-col flex-wrap items-center gap-1"
+                          onClick={() => setSelectedGuild(guild.guild)}
+                        >
+                          <div className="rounded-md p-2">
+                            <Avatar
+                              isBordered
+                              src={guild.guild.image.imageUrl}
+                              alt="guild-image"
+                              className="h-[40px] w-[40px]"
+                            />
+                          </div>
+                          <p className="text-xs">{guild.guild.name}</p>
                         </div>
-                        <p className="text-xs">{guild.guild.name}</p>
-                      </div>
-                    ))}
+                      ))}
+                    </div>
                   </div>
-                </div>
 
-                <div className="flex flex-col gap-2">
-                  <h3 className="text-sm font-semibold text-navTextColor">
-                    Others
-                  </h3>
+                  <div className="flex flex-col gap-2">
+                    <h3 className="text-sm font-semibold text-navTextColor">
+                      Others
+                    </h3>
 
-                  <div className="flex gap-4">
-                    <div className="flex flex-col flex-wrap items-center gap-1">
-                      <Button
-                        isIconOnly
-                        onClick={handleCopyToClipboard}
-                        color="secondary"
-                        variant="bordered"
-                        className="rounded-xl text-lg"
-                        size="md"
-                      >
-                        {isCopied ? <FaCheck /> : <LuCopy />}
-                      </Button>
-                      <p className="text-xs">Copy link</p>
+                    <div className="flex gap-4">
+                      <div className="flex flex-col flex-wrap items-center gap-1">
+                        <Button
+                          isIconOnly
+                          onClick={handleCopyToClipboard}
+                          color="secondary"
+                          variant="bordered"
+                          className="rounded-xl text-lg"
+                          size="md"
+                        >
+                          {isCopied ? <FaCheck /> : <LuCopy />}
+                        </Button>
+                        <p className="text-xs">Copy link</p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              )}
 
-              {/* <div>
-                  <h3 className="text-sm font-semibold text-navTextColor">
-                    Notification
-                  </h3>
-                </div> */}
+              {/* left */}
+              {selectedGuild && postToShare && (
+                <SharePostForm
+                  postToShare={postToShare}
+                  guildId={selectedGuild.id}
+                  closeModal={closeModal}
+                />
+              )}
+              {/* </div> */}
             </ModalBody>
-            {/* <ModalFooter>
-              <Button variant="light" onPress={onClose}>
-                Close
-              </Button>
-              <Button color="secondary" onPress={onClose}>
-                Action
-              </Button>
-            </ModalFooter> */}
           </>
         )}
       </ModalContent>
