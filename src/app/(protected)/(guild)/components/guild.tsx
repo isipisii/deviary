d7430e5c71fb31219940636751f2ca7f35259bde
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-02-28 22:13:42 +0800

    Add GuildSharedPostsContainer component

--- src/app/(protected)/(guild)/components/guild.tsx
+++ src/app/(protected)/(guild)/components/guild.tsx
@@ -5,13 +5,14 @@ import { useGetGuildById } from "@/lib/services/guild.api";
 import { useParams } from "next/navigation";
 import GuildDetailsSkeleton from "@/components/skeleton-loaders/guild-details-skeleton";
 import GuildDetails from "./guild-details";
+import GuildSharedPostsContainer from "./guild-shared-posts-container";
 
 export default function Guild() {
   const { guildId } = useParams() as { guildId: string };
   const { data: guild, isLoading, isError } = useGetGuildById(guildId);
 
   return (
-    <div className="relative h-screen w-full overflow-x-hidden">
+    <div className="relative w-full overflow-x-hidden">
       {/* radial gradients */}
       <div
         className="absolute -right-[10rem] -top-[20rem] z-0 h-[600px] w-[800px] opacity-30"
@@ -27,9 +28,8 @@ export default function Guild() {
       />
       {isLoading && <GuildDetailsSkeleton />}
       {/* guild details */}
-      {guild && (
-        <GuildDetails guild={guild} />
-      )}
+      {guild && <GuildDetails guild={guild} />}
+      <GuildSharedPostsContainer guildId={guildId} />
     </div>
   );
 }
