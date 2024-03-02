6b875eb28dc299d7eaf36fa298313c4f436fcab9
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-03-02 23:23:23 +0800

    Add new properties and methods to useModalStore

--- src/lib/store/useModalStore.ts
+++ src/lib/store/useModalStore.ts
@@ -1,20 +1,33 @@
 import { create } from "zustand";
 
 type TSideBarNavState = {
-    isShareModalOpen: boolean
-    openShareModal: () => void
-    closeShareModal: () => void
-    onOpenChangeShareModal: () => void
-    setPostToShare: (post: TPost | null) => void
-    postToShare: TPost | null
-}
+  isShareModalOpen: boolean;
+  openShareModal: () => void;
+  closeShareModal: () => void;
+  onOpenChangeShareModal: () => void;
+  setPostToShare: (post: TPost | null) => void;
+  postToShare: TPost | null;
+
+  isEditSharedPostModalOpen: boolean;
+  openEditSharedPostModal: () => void;
+  onOpenChangeEditSharedPostModal: () => void;
+  sharedPostToEdit: TGuildSharedPost | null;
+  setSharedPostToEdit: (sharedPostToEdit: TGuildSharedPost | null) => void;
+};
 
 export const useModalStore = create<TSideBarNavState>((set, get) => ({
-    isShareModalOpen: false,
-    postToShare: null,
+  isShareModalOpen: false,
+  postToShare: null,
 
-    openShareModal: () => set({isShareModalOpen: true}),
-    closeShareModal: () => set({isShareModalOpen: false}),
-    onOpenChangeShareModal: () => set({ isShareModalOpen: !get().isShareModalOpen }),
-    setPostToShare: (post) => set({ postToShare: post })
-}))
+  sharedPostToEdit: null,
+  isEditSharedPostModalOpen: false,
+
+  openShareModal: () => set({ isShareModalOpen: true }),
+  closeShareModal: () => set({ isShareModalOpen: false }),
+  onOpenChangeShareModal: () => set({ isShareModalOpen: !get().isShareModalOpen }),
+  setPostToShare: (post) => set({ postToShare: post }),
+
+  openEditSharedPostModal: () => set({ isEditSharedPostModalOpen: true }),
+  onOpenChangeEditSharedPostModal: () => set({ isEditSharedPostModalOpen: !get().isEditSharedPostModalOpen }),
+  setSharedPostToEdit: (sharedPostToEdit) => set({ sharedPostToEdit }),
+}));
