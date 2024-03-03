da91a711f35d20506f1e57609b0823d9f11455fc
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-03-03 22:32:33 +0800

    Add GuildMembersModal component

--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -8,6 +8,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ShareModal from "../ui/share-modal";
 import EditSharedPostModal from "@/app/(protected)/(guild)/components/edit-shared-post-modal";
+import GuildMembersModal from "@/app/(protected)/(guild)/components/members/guild-members-modal";
 
 
 export default function Providers({ children }: { children: ReactNode }) {
@@ -29,6 +30,7 @@ export default function Providers({ children }: { children: ReactNode }) {
         <ReactQueryDevtools initialIsOpen={false} />
         <ShareModal />
         <EditSharedPostModal />
+        <GuildMembersModal />
         <NextUIProvider>
           <ThemeProvider
             attribute="class"
