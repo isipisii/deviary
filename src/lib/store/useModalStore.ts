2ed55eca6de1b13ae1e64325407e198f28e81688
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-03-03 22:31:31 +0800

    Add modal state for guild members

--- src/lib/store/useModalStore.ts
+++ src/lib/store/useModalStore.ts
@@ -1,6 +1,8 @@
+import { boolean } from "zod";
 import { create } from "zustand";
 
-type TSideBarNavState = {
+type TModalStoreState = {
+  // for share post modal 
   isShareModalOpen: boolean;
   openShareModal: () => void;
   closeShareModal: () => void;
@@ -8,26 +10,39 @@ type TSideBarNavState = {
   setPostToShare: (post: TPost | null) => void;
   postToShare: TPost | null;
 
+  // for editing shared post modal 
   isEditSharedPostModalOpen: boolean;
   openEditSharedPostModal: () => void;
   onOpenChangeEditSharedPostModal: () => void;
   sharedPostToEdit: TGuildSharedPost | null;
   setSharedPostToEdit: (sharedPostToEdit: TGuildSharedPost | null) => void;
+
+  isGuildMembersModalOpen: boolean
+  openGuildMembersModal: () => void;
+  onOpenChangeGuildMembersModal: () => void;
 };
 
-export const useModalStore = create<TSideBarNavState>((set, get) => ({
+export const useModalStore = create<TModalStoreState>((set, get) => ({
+  // for share post modal 
   isShareModalOpen: false,
   postToShare: null,
-
-  sharedPostToEdit: null,
-  isEditSharedPostModalOpen: false,
 
   openShareModal: () => set({ isShareModalOpen: true }),
   closeShareModal: () => set({ isShareModalOpen: false }),
   onOpenChangeShareModal: () => set({ isShareModalOpen: !get().isShareModalOpen }),
   setPostToShare: (post) => set({ postToShare: post }),
 
+  // for editing shared post modal 
+  sharedPostToEdit: null,
+  isEditSharedPostModalOpen: false,
+
   openEditSharedPostModal: () => set({ isEditSharedPostModalOpen: true }),
   onOpenChangeEditSharedPostModal: () => set({ isEditSharedPostModalOpen: !get().isEditSharedPostModalOpen }),
   setSharedPostToEdit: (sharedPostToEdit) => set({ sharedPostToEdit }),
+
+  //for guild members modal 
+  isGuildMembersModalOpen: false,
+
+  openGuildMembersModal: () => set({ isGuildMembersModalOpen: true }),
+  onOpenChangeGuildMembersModal: () => set({ isGuildMembersModalOpen: !get().isGuildMembersModalOpen }),
 }));
