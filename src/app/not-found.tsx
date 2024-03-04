301f1895d88468a37adf41c8a1535aef85d77379
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-03-04 23:27:39 +0800

    Add 404 page with link to feed

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,23 @@
+import Link from "next/link";
+import { TbError404 } from "react-icons/tb";
 
 export default function NotFound() {
   return (
-    <div>
-      not found
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="flex flex-col items-center justify-between gap-4">
+        <p className="text-center text-[15rem] text-secondary">
+          <TbError404 />{" "}
+        </p>
+        <div>
+          <h2 className="text-center text-[2rem] font-black">Not Found</h2>
+          <p className="text-center text-navTextColor">Nothing to see here.</p>
+        </div>
+        <Link href="/feed">
+          <p className="hover:bg-secondary/20 transition ease-in-out duration-500 rounded-md px-4 py-2 font-semibold text-secondary">
+            Go to feed instead?
+          </p>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
