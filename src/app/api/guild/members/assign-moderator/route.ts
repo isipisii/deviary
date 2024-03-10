fbd902d6e72ea714a17a1ae8153a0d95201de02f
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-03-10 21:18:48 +0800

    Refactor assign-moderator route and add delete functionality

--- src/app/api/guild/members/assign-moderator/route.ts
+++ src/app/api/guild/members/assign-moderator/route.ts
@@ -3,7 +3,7 @@ import { db } from "@/lib/prisma";
 import { checkIfGuildCreator } from "@/utils/checkGuildActionsEligibility";
 import { NextResponse, NextRequest } from "next/server";
 
-export const PATCH = async (req: NextRequest) => {
+export const POST = async (req: NextRequest) => {
     const session = await getServerSideSession()
     const url = new URL(req.url)
     const guildId = url.searchParams.get("guildId")
@@ -67,6 +67,65 @@ export const PATCH = async (req: NextRequest) => {
 
         return NextResponse.json({
             message: "Member was succesfully assigned as moderator",
+            success: true
+        }, { status: 200 })
+
+    } catch (error) {
+        return NextResponse.json({
+            message: "Internal Server Error",
+            success: false
+        }, { status: 500 })
+    }
+}
+
+
+export const DELETE = async (req: NextRequest) => {
+    const session = await getServerSideSession()
+    const url = new URL(req.url)
+    const memberId = url.searchParams.get("memberId")
+
+    try {
+        if (!session) {
+            return NextResponse.json(
+              {
+                success: false,
+                message: "Unauthenticated",
+              },
+              { status: 401 },
+            );
+          }
+
+        if(!memberId) {
+            return NextResponse.json({
+                message: "Missing member id",
+                success: false
+            }, { status: 400 })
+        }
+
+        const existingMember = await db.guildMember.findUnique({
+            where: {
+                id: memberId
+            }
+        })
+        
+        if(!existingMember) {
+            return NextResponse.json({
+                message: "This user is not a member of the guild",
+                success: false
+            }, { status: 404 })
+        }
+
+        await db.guildMember.update({
+            where: {
+                id: existingMember.id 
+            }, 
+            data: {
+                role: "MEMBER"
+            }
+        })
+
+        return NextResponse.json({
+            message: "Moderator was succesfully removed",
             success: true
         }, { status: 200 })
 
