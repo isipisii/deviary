4649a74f27faebd747c8787069ec87a2e03a3b4f
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-03-10 21:19:38 +0800

    Refactor member card component to handle moderator assignment and removal

--- src/app/(protected)/(guild)/components/members/member-card.tsx
+++ src/app/(protected)/(guild)/components/members/member-card.tsx
@@ -2,20 +2,38 @@
 
 import formatDate from "@/utils/formatDate";
 import { Avatar, Button } from "@nextui-org/react";
-import { useRemoveGuildMember } from "@/lib/services/guild.api";
+import {
+  useAssignModerator,
+  useRemoveGuildMember,
+  useUnassignModerator,
+} from "@/lib/services/guild.api";
 import { useQueryClient } from "@tanstack/react-query";
 import { QueryKeys } from "@/lib/constants";
+import { useSession } from "next-auth/react";
 
 export default function MemberCard({
   guildMember,
 }: {
   guildMember: TGuildMember;
 }) {
-  const queryClient = useQueryClient()
-  const { user, id, createdAt, role, guildId } = guildMember;
-  const currentGuild = queryClient.getQueryData<TGuild>([QueryKeys.Guild, guildId])
+  const { data: sessionData } = useSession();
+  const queryClient = useQueryClient();
+  const { user, id: memberId, createdAt, role, guildId } = guildMember;
+  const currentGuild = queryClient.getQueryData<TGuild>([
+    QueryKeys.Guild,
+    guildId,
+  ]);
   const { mutate: removeMemberMutation, isPending: isRemovingMember } =
     useRemoveGuildMember();
+  const { mutate: assignModeratorMutation, isPending: isAssigning } =
+    useAssignModerator();
+  const { mutate: unassignModeratorMutation, isPending: isUnassigning } =
+    useUnassignModerator();
+
+  function handleAssignModerator() {
+    if (role === "MEMBER") assignModeratorMutation({ guildId, memberId });
+    if (role === "MODERATOR") unassignModeratorMutation({ guildId, memberId });
+  }
 
   return (
     <div className="flex w-full items-center justify-between gap-8 py-3">
@@ -23,7 +41,9 @@ export default function MemberCard({
         <Avatar src={user.image} isBordered className="h-[35px] w-[35px]" />
         <div>
           <div className="flex items-center gap-1">
-            <p className="text-sm font-semibold">{user.name}</p>
+            <p className="text-sm font-semibold">
+              {sessionData?.user.id === user.id ? "You" : user.name}
+            </p>
             {role === "CREATOR" && (
               <p className="rounded-lg bg-warning/20 px-2 py-1 text-[.7rem] font-semibold text-warning">
                 Creator
@@ -47,31 +67,40 @@ export default function MemberCard({
         </div>
       </div>
 
-      <div className="flex gap-2">
-        {role === "MEMBER" && currentGuild?.isBelong && (
-          <Button
-            color="secondary"
-            size="sm"
-            variant="ghost"
-            className="hover:text-white"
-            radius="lg"
-          >
-            Assign Mod
-          </Button>
+      {/* will only show if this member is not equal to the authenticated user 
+      and if the authenticated user is either creator or moderator of the guild */}
+      {sessionData?.user.id !== user.id &&
+        (currentGuild?.isModerator || currentGuild?.isGuildCreator) && (
+          <div className="flex gap-2">
+            {currentGuild?.isBelong && currentGuild?.isGuildCreator && (
+              <Button
+                color="secondary"
+                size="sm"
+                variant="ghost"
+                className="hover:text-white"
+                radius="lg"
+                isLoading={isAssigning || isUnassigning}
+                onClick={handleAssignModerator}
+              >
+                {role === "MODERATOR" ? "Remove Mod" : "Assign Mod"}
+              </Button>
+            )}
+            {role !== "CREATOR" &&
+              currentGuild?.isBelong &&
+              currentGuild?.isModerator || currentGuild?.isGuildCreator && (
+                <Button
+                  onClick={() => removeMemberMutation({ guildId, memberId })}
+                  color="danger"
+                  variant="ghost"
+                  size="sm"
+                  radius="lg"
+                  isLoading={isRemovingMember}
+                >
+                  Remove
+                </Button>
+              )}
+          </div>
         )}
-        {role !== "CREATOR" && currentGuild?.isBelong && (
-          <Button
-            onClick={() => removeMemberMutation({ guildId, memberId: id })}
-            color="danger"
-            variant="ghost"
-            size="sm"
-            radius="lg"
-            isLoading={isRemovingMember}
-          >
-            Remove
-          </Button>
-        )}
-      </div>
     </div>
   );
 }
