5955a7f03b699e6aa23437a6e88753f3181bab00
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-03-12 22:47:46 +0800

    Add pusher server integration for accepting join requests

--- src/app/api/guild/join-request/accept/route.ts
+++ src/app/api/guild/join-request/accept/route.ts
@@ -1,6 +1,9 @@
 import { db } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSideSession } from "@/lib/auth";
+import { getPusherInstance } from "@/lib/pusher/server";
+
+const pusherServer = getPusherInstance()
 
 // accept the request
 export const POST = async (req: NextRequest) => {
@@ -8,8 +11,6 @@ export const POST = async (req: NextRequest) => {
   const joinRequestId = url.searchParams.get("joinRequestId") as string;
   const session = await getServerSideSession();
   const authenticatedUserId = session?.user.id;
-
-  console.log(joinRequestId)
 
   try {
     if (!session) {
@@ -66,11 +67,45 @@ export const POST = async (req: NextRequest) => {
     }
 
     // create a new guild member
-    await db.guildMember.create({
+    const newGuildMember = await db.guildMember.create({
       data: {
         userId: existingRequest.senderId,
         guildId: existingRequest.guildId,
       },
+      include: {
+        user: {
+          select: {
+            id: true
+          }
+        }
+      }
+    });
+
+    const notification = await db.notification.create({
+      data: {
+        recipientId: newGuildMember.user.id,
+        senderId: authenticatedUserId,
+        type: "JOIN_REQUEST_ACCEPTED",
+        guildId: newGuildMember.guildId,
+      },
+      include: {
+        sender: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            image: true,
+          }
+        },
+        guild: true,
+      }
+    })
+
+    const channel = `channel_user_${notification.recipientId}`;
+    const event = "new-notification";
+
+    await pusherServer.trigger(channel, event, {
+      notification,
     });
 
     // delete the join request
