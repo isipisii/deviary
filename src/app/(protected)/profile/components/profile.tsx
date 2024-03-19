af5cff851cb3fbd5b3c45ba672e6c7e82e1aaa3b
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-03-19 13:16:56 +0800

    Refactor profile details and add user profile tabs

--- src/app/(protected)/profile/components/profile.tsx
+++ src/app/(protected)/profile/components/profile.tsx
@@ -2,17 +2,18 @@
 import ProfileDetails from "./profile-details";
 import { useGetUserById } from "@/lib/services/user.api";
 import { useParams } from "next/navigation";
+import UserProfileTabs from "./user-profile-tabs";
 
 export default function Profile() {
   const { userId } = useParams<{ userId: string }>();
   const { data } = useGetUserById(userId);
   // TODO: CREATE A UI FOR UPDATING THE USER PROFILE
   return (
-    <div className="h-screen w-full">
+    <div className="h-screen w-full flex flex-col gap-4">
       {/* profile details */}
       {data && <ProfileDetails user={data} />}
       {/* user posts and upvotes */}
-      <div></div>
+      <UserProfileTabs />
     </div>
   );
 }
