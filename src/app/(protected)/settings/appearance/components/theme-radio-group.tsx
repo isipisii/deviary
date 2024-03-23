46a248871a376c5bf5b4f856a0ed3d27d2ad8feb
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-03-23 14:00:52 +0800

    Add ThemeRadioGroup component

---
+++ src/app/(protected)/settings/appearance/components/theme-radio-group.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { CustomRadio } from "@/components/ui/custom-radio";
+import { RadioGroup } from "@nextui-org/react";
+import { useTheme } from "next-themes";
+
+export default function ThemeRadioGroup() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <RadioGroup value={theme} onValueChange={setTheme}>
+      <CustomRadio value="light" size="sm" color="secondary">
+        Light
+      </CustomRadio>
+      <CustomRadio value="dark" size="sm" color="secondary">
+        Dark
+      </CustomRadio>
+      <CustomRadio value="system" size="sm" color="secondary">
+        System
+      </CustomRadio>
+    </RadioGroup>
+  );
+}
