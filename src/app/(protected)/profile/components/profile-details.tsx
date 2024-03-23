805e31bf0d27482b475ebf6af1d3b97eb64705cd
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2024-03-23 14:01:24 +0800

    Refactor profile details component layout

--- src/app/(protected)/profile/components/profile-details.tsx
+++ src/app/(protected)/profile/components/profile-details.tsx
@@ -3,32 +3,32 @@ import { Avatar, Image } from "@nextui-org/react";
 import { FaGithub, FaFacebook } from "react-icons/fa";
 import formatDate from "@/utils/formatDate";
 
-// TODO: ADD BG IMAGE
 export default function ProfileDetails({ user }: { user?: TUser }) {
-
   function getSocialUsername(url: string) {
     return new URL(url).pathname.split("").slice(1).join("");
   }
-  
+
   return (
-    <div className="flex w-full flex-col gap-3">
+    <div className="flex w-full flex-col gap-4">
       <div className="w-full">
         <Image
           isBlurred
-          src="https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg"
-          alt="thumbnail"
+          src={user?.backgroundImage?.imageUrl}
+          alt="Cover image"
           removeWrapper
           className="left-0 top-0 z-[0] h-[150px] w-full rounded-xl object-cover"
         />
 
-        <div className="flex flex-col z-[10] -mt-10 px-4 gap-3">
+        <div className="z-[10] -mt-10 flex flex-col gap-3 px-4">
           <Avatar
             src={user?.image}
             isBordered
-            className="md:h-[130px] md:w-[130px] h-[90px] w-[90px]"
+            className="h-[90px] w-[90px] md:h-[130px] md:w-[130px]"
           />
           <div>
-            <h2 className="md:text-2xl text-[1.4rem] font-bold">{user?.name}</h2>
+            <h2 className="text-[1.4rem] font-bold md:text-2xl">
+              {user?.name}
+            </h2>
             <p className="text-sm">
               @{user?.name.toLowerCase()}{" "}
               <span className="font-semibold text-navTextColor">
@@ -40,35 +40,37 @@ export default function ProfileDetails({ user }: { user?: TUser }) {
         </div>
       </div>
 
-      <div className="flex flex-col gap-2 px-4">
+      <div className="flex flex-col gap-4 px-4">
         {user?.bio && <p>{user?.bio}</p>}
         <div className="flex gap-2">
           {user?.social?.github && (
-            <div className="flex items-center gap-1 rounded-lg border border-borderColor px-3 py-2">
-              <a
-                className="text-xl text-navTextColor"
-                href={user?.social?.github}
-              >
+            <a
+              href={user?.social?.github}
+              target="_blank"
+              className="flex items-center gap-1 rounded-lg border border-borderColor px-3 py-2"
+            >
+              <p className="text-xl text-navTextColor">
                 <FaGithub />
-              </a>
+              </p>
               <p className="text-sm text-navTextColor">
                 @{getSocialUsername(user.social.github)}
               </p>
-            </div>
+            </a>
           )}
 
           {user?.social?.facebook && (
-            <div className="flex items-center gap-1 rounded-lg border border-borderColor px-3 py-2">
-              <a
-                className="text-xl text-navTextColor"
-                href={user?.social?.facebook}
-              >
+            <a
+              href={user?.social?.facebook}
+              target="_blank"
+              className="flex items-center gap-1 rounded-lg border border-borderColor px-3 py-2"
+            >
+              <p className="text-xl text-navTextColor">
                 <FaFacebook />
-              </a>
+              </p>
               <p className="text-sm text-navTextColor">
                 @{getSocialUsername(user.social.facebook)}
               </p>
-            </div>
+            </a>
           )}
         </div>
       </div>
